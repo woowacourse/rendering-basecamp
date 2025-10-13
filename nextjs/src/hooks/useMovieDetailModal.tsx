69945844bf4c66a3b1e4542e5af7cc9364003532
deokbin <deokbin@naver.com>
2025-10-13 17:39:55 +0900

    chore: 컴포넌트 코드 옮기기

---
+++ nextjs/src/hooks/useMovieDetailModal.tsx
@@ -0,0 +1,21 @@
+import { overlay } from 'overlay-kit';
+import { MovieDetailResponse } from '../types/MovieDetail.types';
+import { MovieDetailModal } from '../components/MovieDetailModal';
+
+export const useMovieDetailModal = () => {
+  const openMovieDetailModal = (movie: MovieDetailResponse) => {
+    return new Promise<void>(resolve => {
+      overlay.open(({ unmount }) => (
+        <MovieDetailModal
+          movie={movie}
+          onClose={() => {
+            resolve();
+            unmount();
+          }}
+        />
+      ));
+    });
+  };
+
+  return { openMovieDetailModal };
+};
