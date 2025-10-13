c5adc722f1e17a5cc4cfee42e627836f7c0b110c
guesung <gueit214@naver.com>
2025-10-13 14:57:24 +0900

    chore: og:url, og:image 추가

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -15,6 +15,10 @@ export default function MovieHomePage({ movies }: MovieHomePageProps) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
+  const ogImageUrl = movies[0]?.backdrop_path
+    ? `https://image.tmdb.org/t/p/original${movies[0].backdrop_path}`
+    : `https://image.tmdb.org/t/p/original${movies[0]?.poster_path}`;
+
   return (
     <>
       <Head>
@@ -23,6 +27,11 @@ export default function MovieHomePage({ movies }: MovieHomePageProps) {
           name="description"
           content="최신 인기 영화 정보를 확인하세요. TMDB 기반 영화 데이터베이스"
           key="description"
+        />
+        <meta
+          property="og:url"
+          content="https://rendering-basecamp-git-guesung-gueit214s-projects.vercel.app/"
+          key="og:url"
         />
         <meta
           property="og:title"
@@ -35,6 +44,9 @@ export default function MovieHomePage({ movies }: MovieHomePageProps) {
           key="og:description"
         />
         <meta property="og:type" content="website" key="og:type" />
+        <meta property="og:image" content={ogImageUrl} key="og:image" />
+        <meta property="og:image:width" content="1280" key="og:image:width" />
+        <meta property="og:image:height" content="720" key="og:image:height" />
         <meta
           name="twitter:card"
           content="summary_large_image"
@@ -50,6 +62,7 @@ export default function MovieHomePage({ movies }: MovieHomePageProps) {
           content="최신 인기 영화 정보를 확인하세요"
           key="twitter:description"
         />
+        <meta name="twitter:image" content={ogImageUrl} key="twitter:image" />
       </Head>
       <div id="wrap">
         <Header featuredMovie={movies[0]} />
