1a97922758a4ce2e9669924d2632a5336258e6ba
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-14 15:41:54 +0900

    feat: Open Graph Tag 개선

--- nextjs/src/pages/detail/[movieId].tsx
+++ nextjs/src/pages/detail/[movieId].tsx
@@ -1,12 +1,27 @@
 import { useEffect, useRef } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import MovieHomePage from "../MovieHomePage";
 import { moviesApi } from "../../api/movies";
 import { useMovieDetailModal } from "../../hooks/useMovieDetailModal";
 
-export default function MovieDetailPage() {
+export default function MovieDetailPage({
+  movieForOg,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
+      <Head>
+        <title>{movieForOg.title}</title>
+        <meta name="description" content={movieForOg.overview} />
+        <meta property="og:title" content={movieForOg.title} />
+        <meta property="og:description" content={movieForOg.overview} />
+        <meta property="og:image" content={movieForOg.ogImage} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={movieForOg.title} />
+        <meta property="twitter:description" content={movieForOg.overview} />
+        <meta property="twitter:image" content={movieForOg.ogImage} />
+      </Head>
       <MovieHomePage />
       <DetailPageOpenModal />
     </>
@@ -32,3 +47,40 @@ function DetailPageOpenModal() {
 
   return null;
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  movieForOg: { title: string; overview: string; ogImage: string };
+}> = async (ctx) => {
+  const { movieId } = ctx.query;
+  if (typeof movieId !== "string") {
+    return { notFound: true };
+  }
+
+  try {
+    const res = await moviesApi.getDetail(Number(movieId));
+    const movie = res.data;
+
+    const proto = (ctx.req.headers["x-forwarded-proto"] as string) ?? "http";
+    const host =
+      (ctx.req.headers["x-forwarded-host"] as string) ??
+      ctx.req.headers.host ??
+      "localhost:3000";
+    const origin = `${proto}://${host}`;
+
+    const ogImage = movie.poster_path
+      ? `https://image.tmdb.org/t/p/w780${movie.poster_path}`
+      : `${origin}/images/no_image.png`;
+
+    return {
+      props: {
+        movieForOg: {
+          title: movie.title,
+          overview: movie.overview ?? "",
+          ogImage,
+        },
+      },
+    };
+  } catch {
+    return { notFound: true };
+  }
+};
