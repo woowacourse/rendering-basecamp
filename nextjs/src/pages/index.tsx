32173f51c2b6037beba61bdcc54eccef8a943582
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-14 18:06:19 +0900

    feat: 영화 상세 정보에 따라 동적으로 Open Graph 및 Twitter 메타 태그 추가

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -12,14 +12,32 @@ const Home: NextPageWithLayout<{
   movieForOg?: { title: string; overview: string; ogImage: string } | null;
 }> = ({
   initialMovies,
+  movieForOg,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <Head>
-        <title>인기 영화</title>
-        <meta name="description" content="TMDB 인기 영화 목록" />
+        <title>{movieForOg ? movieForOg.title : "인기 영화"}</title>
+        <meta
+          name="description"
+          content={movieForOg ? movieForOg.overview : "TMDB 인기 영화 목록"}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {movieForOg && (
+          <>
+            <meta property="og:title" content={movieForOg.title} />
+            <meta property="og:description" content={movieForOg.overview} />
+            <meta property="og:image" content={movieForOg.ogImage} />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:title" content={movieForOg.title} />
+            <meta
+              property="twitter:description"
+              content={movieForOg.overview}
+            />
+            <meta property="twitter:image" content={movieForOg.ogImage} />
+          </>
+        )}
       </Head>
       <HomeView initialMovies={initialMovies} />
     </>
