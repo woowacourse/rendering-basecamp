20b9ab49f19885d5f680fcf6aeeba610b6e1172d
sooyeoniya <tndus1306403@naver.com>
2025-10-14 16:50:11 +0900

    chore: html lang 설정 추가
    _document.tsx 파일 삭제됨에 따라 <Html lang="ko"> 설정을 nextConfig 에서 처리하도록 함

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko",
+  },
 };
 
 export default nextConfig;
