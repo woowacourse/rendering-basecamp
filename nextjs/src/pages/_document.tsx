e4ac475f9fb2d6aece558eda81ea0289b0b2fbdc
SuHwa Lee <tnghk9611@naver.com>
2025-10-14 16:38:01 +0900

    chore: 메타 태그 추가

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta name="description" content="지금 인기 있는 영화를 확인하세요" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="영화 리뷰 서비스" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { MovieList } from "@/components/MovieList";
 import { MovieItem } from "@/types/Movie.types";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 
 interface HomeProps {
   movies: MovieItem[];
@@ -14,12 +15,25 @@ export default function Home({ movies }: HomeProps) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
+  const ogContent = "보고싶은 영화의 리뷰를 확인하세요";
+  const ogImage = `https://image.tmdb.org/t/p/w1280${movies[0].poster_path}`;
+
   return (
-    <div id="wrap">
-      <Header featuredMovie={movies[0]} />
-      <MovieList movies={movies} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>영화 리뷰 서비스</title>
+        <meta name="description" content={ogContent} />
+        <meta property="og:title" content={ogContent} />
+        <meta property="og:description" content={ogContent} />
+        <meta property="og:image" content={ogImage} />
+      </Head>
+
+      <div id="wrap">
+        <Header featuredMovie={movies[0]} />
+        <MovieList movies={movies} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
