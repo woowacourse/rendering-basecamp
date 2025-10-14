d4e05f9864f6a08cbd03d07bc5c103118c5ce1d8
sooyeoniya <tndus1306403@naver.com>
2025-10-14 16:16:57 +0900

    chore: 문자 인코딩 방식 지정
    charSet과 lang 차이 정리
    
    <meta charSet="utf-8" />
    - 페이지의 문자 인코딩 방식 지정
    - 브라우저가 텍스트를 올바르게 해석할 수 있도록 함
    - 한글, 이모지 등이 깨지지 않고 정상적으로 표시됨
    
    <Html lang="ko">
    - 문서의 기본 언어 설정
    - 브라우저, 스크린 리더, 검색 엔진 등이 이 정보를 활용
    - 접근성 향상, SEO 개선, 자동 번역 지원에 유리

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
