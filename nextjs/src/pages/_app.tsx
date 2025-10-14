c01caced68688f676c0210ab56666d7c01624713
sooyeoniya <tndus1306403@naver.com>
2025-10-14 20:18:34 +0900

    feat: OverlayProvider 적용

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { OverlayProvider } from "overlay-kit";
+import "../styles/index.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <OverlayProvider>
+        <Component {...pageProps} />
+      </OverlayProvider>
     </>
   );
 }
