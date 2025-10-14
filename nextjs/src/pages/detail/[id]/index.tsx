39258d2431b1e6df29a1c38d462d0a01326c3d91
wo-o29 <woogur29@gmail.com>
2025-10-14 20:45:04 +0900

    refactor: 상세 페이지 getServerSideProps 적용

--- nextjs/src/pages/detail/[id]/index.tsx
+++ nextjs/src/pages/detail/[id]/index.tsx
@@ -1,37 +1,54 @@
 import { moviesApi } from "@/api/movies";
+import Meta from "@/components/common/Meta";
 import { useMovieDetailModal } from "@/hooks/useMovieDetailModal";
 import MovieHomePage from "@/pages";
-import { useRouter } from "next/router";
-import { useRef, useEffect } from "react";
+import { MovieDetailResponse } from "@/types/MovieDetail.types";
+import { GetServerSidePropsContext } from "next";
+import { useEffect } from "react";
 
-export default function MovieDetailPage() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  try {
+    const movieDetailResponse = await moviesApi.getDetail(
+      Number(context.query?.id)
+    );
+
+    return {
+      props: { movieDetail: movieDetailResponse.data },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+interface MovieDetailPageProps {
+  movieDetail: MovieDetailResponse;
+}
+
+export default function MovieDetailPage({ movieDetail }: MovieDetailPageProps) {
+  const { openMovieDetailModal } = useMovieDetailModal();
+
+  useEffect(() => {
+    if (movieDetail) {
+      openMovieDetailModal(movieDetail);
+    }
+  }, [movieDetail, openMovieDetailModal]);
+
+  const imageUrl = movieDetail.poster_path
+    ? `https://image.tmdb.org/t/p/original${movieDetail.poster_path}`
+    : "/images/no_image.png";
+
   return (
     <>
       <MovieHomePage />
-      <DetailPageOpenModal />
+      <Meta
+        title={movieDetail.title}
+        description={movieDetail.overview}
+        imgUrl={imageUrl}
+      />
     </>
   );
-}
-
-function DetailPageOpenModal() {
-  const { query } = useRouter();
-  const movieId = query.id;
-
-  const { openMovieDetailModal } = useMovieDetailModal();
-  const onceRef = useRef(false);
-
-  useEffect(() => {
-    if (movieId == null || onceRef.current === true) {
-      return;
-    }
-
-    (async () => {
-      onceRef.current = true;
-
-      const movieDetail = await moviesApi.getDetail(Number(movieId));
-      openMovieDetailModal(movieDetail.data);
-    })();
-  }, [movieId, openMovieDetailModal]);
-
-  return null;
 }
