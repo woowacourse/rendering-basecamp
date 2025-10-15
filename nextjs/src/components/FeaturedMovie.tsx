aa90f56555b612a348b0f6c13808954b7bf75cc7
mun-kyeong <horse6953@naver.com>
2025-10-16 03:26:05 +0900

    feat: '자세히 보기'버튼 - 라우터를 사용하여 상세 페이지로 이동

--- nextjs/src/components/FeaturedMovie.tsx
+++ nextjs/src/components/FeaturedMovie.tsx
@@ -1,7 +1,8 @@
-import { useMovieDetailModal } from '../hooks/useMovieDetailModal';
-import { Button } from './common/Button';
-import type { MovieItem } from '../types/Movie.types';
-import { moviesApi } from '../api/movies';
+import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import { Button } from "./common/Button";
+import type { MovieItem } from "../types/Movie.types";
+import { moviesApi } from "../api/movies";
+import { useRouter } from "next/router";
 
 interface FeaturedMovieProps {
   movie: MovieItem;
@@ -9,10 +10,10 @@ interface FeaturedMovieProps {
 
 export const FeaturedMovie = ({ movie }: FeaturedMovieProps) => {
   const { openMovieDetailModal } = useMovieDetailModal();
+  const router = useRouter();
 
   const handleDetailClick = async () => {
-    const movieDetail = await moviesApi.getDetail(movie.id);
-    await openMovieDetailModal(movieDetail.data);
+    router.push(`/detail/${movie.id}`);
   };
 
   return (
