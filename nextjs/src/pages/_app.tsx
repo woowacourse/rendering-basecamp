92b7bb4cae004bec4621215586fb98e5b7a5a62b
이상희 <sang6299@naver.com>
2025-10-16 07:29:42 +0900

    refactor: OverlayProvider 제거

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import type { AppProps } from 'next/app';
-import { OverlayProvider } from 'overlay-kit';
 import '../index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <OverlayProvider>
-      <Component {...pageProps} />
-    </OverlayProvider>
-  );
+  return <Component {...pageProps} />;
 }
