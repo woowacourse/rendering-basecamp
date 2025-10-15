959d0237deb433d974a21aa7929ddd0c76f79ebe
이상희 <sang6299@naver.com>
2025-10-16 06:54:51 +0900

    feat: getServerSideProps 적용

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { Header } from '../components/Header';
 import { MovieList } from '../components/MovieList';
 import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
+import { moviesApi } from '../api/movies';
+import { MovieItem } from '../types/Movie.types';
 
-export default function Home() {
-  const { data: movies, isLoading } = usePopularMovies();
+interface HomePageProps {
+  movies: MovieItem[];
+}
 
-  if (isLoading === true) {
-    return <Loading />;
-  }
-
+export default function Home({ movies }: HomePageProps) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
@@ -32,3 +31,26 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<
+  HomePageProps
+> = async () => {
+  try {
+    const movieResponse = await moviesApi.getPopular();
+    const movies = movieResponse.data.results;
+
+    return {
+      props: {
+        movies,
+      },
+    };
+  } catch (error) {
+    console.error('영화 데이터 페칭 실패:', error);
+
+    return {
+      props: {
+        movies: [],
+      },
+    };
+  }
+};
