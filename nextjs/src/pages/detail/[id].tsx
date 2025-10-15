66dbc3c06c42b400a5e2f0460b4fb46a2b41a06b
AHHYUNJU <wndkgus0225@naver.com>
2025-10-16 01:27:34 +0900

    feat: 각 페이지마다 getServerSideProps 추가

--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -2,7 +2,30 @@ import { moviesApi } from '@/api/movies';
 import { useMovieDetailModal } from '@/hooks/useMovieDetailModal';
 import { useRouter } from 'next/router';
 import { useRef, useEffect } from 'react';
-import MovieHomePage from '..';
+import Head from 'next/head';
+import type { MovieDetailResponse } from '@/types/MovieDetail.types';
+
+export const getServerSideProps = (async (context) => {
+  const { id } = context.params as { id: string };
+
+  try {
+    const movieDetailResponse = await moviesApi.getDetail(Number(id));
+    const movieDetail = movieDetailResponse.data;
+
+    return {
+      props: {
+        movieDetail,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch movie data:', error);
+    return {
+      notFound: true,
+    };
+  }
+}) satisfies GetServerSideProps<{
+  movieDetail: MovieDetailResponse;
+}>;
 
 export default function MovieDetailPage() {
   return (
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,25 +1,48 @@
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 import { Header } from '../components/Header';
 import { MovieList } from '../components/MovieList';
 import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
+import Head from 'next/head';
+import { moviesApi } from '../api/movies';
+import type { MovieItem } from '../types/Movie.types';
 
-export default function MovieHomePage() {
-  const { data: movies, isLoading } = usePopularMovies();
+export const getServerSideProps = (async () => {
+  try {
+    const movieResponse = await moviesApi.getPopular();
+    const movies = movieResponse.data.results;
 
-  if (isLoading === true) {
-    return <Loading />;
+    return {
+      props: {
+        movies,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch movies:', error);
+    return {
+      props: {
+        movies: [],
+      },
+    };
   }
+}) satisfies GetServerSideProps<{ movies: MovieItem[] }>;
 
+export default function MovieHomePage({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
   return (
-    <div id="wrap">
-      <Header featuredMovie={movies[0]} />
-      <MovieList movies={movies} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>영화 추천 사이트</title>
+      </Head>
+      <div id="wrap">
+        <Header featuredMovie={movies[0]} />
+        <MovieList movies={movies} />
+        <Footer />
+      </div>
+    </>
   );
 }
