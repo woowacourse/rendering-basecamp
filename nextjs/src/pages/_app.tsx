a4b221f55571cf862d1ae9859aad4cc0f9b84276
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-15 19:47:08 +0900

    feat: 스타일 파일 추가 및 기본 스타일 설정

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app"
 import { OverlayProvider } from "overlay-kit"
+import "./index.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
