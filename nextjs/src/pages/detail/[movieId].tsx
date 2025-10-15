a3c26b10bdb605a6f9313b1a73e58ea24bc27ce7
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-15 22:38:41 +0900

    feat: 영화 상세 페이지 및 모달 기능 추가

---
+++ nextjs/src/pages/detail/[movieId].tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from "react"
+
+import { moviesApi } from "@/api/movies"
+import Home from "@/pages"
+
+import { MovieItem } from "@/types/Movie.types"
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next"
+
+import { useMovieDetailModal } from "@/hooks/useMovieDetailModal"
+import { MovieDetailResponse } from "@/types/MovieDetail.types"
+
+export const getServerSideProps = (async (context) => {
+  const { movieId } = context.query
+  if (!movieId || typeof movieId !== "string") {
+    return { notFound: true }
+  }
+
+  try {
+    const movies = await moviesApi.getPopular()
+    const movieDetail = await moviesApi.getDetail(Number(movieId))
+
+    return {
+      props: { movies: movies.data.results, movieDetail: movieDetail.data },
+    }
+  } catch (error) {
+    return { notFound: true }
+  }
+}) satisfies GetServerSideProps<{
+  movies: MovieItem[]
+  movieDetail: MovieDetailResponse
+}>
+
+export default function MovieDetailPage({
+  movies,
+  movieDetail,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <Home movies={movies} />
+      <DetailPageOpenModal movieDetail={movieDetail} />
+    </>
+  )
+}
+
+function DetailPageOpenModal({
+  movieDetail,
+}: {
+  movieDetail: MovieDetailResponse
+}) {
+  const { openMovieDetailModal } = useMovieDetailModal()
+  const onceRef = useRef(false)
+
+  useEffect(() => {
+    if (onceRef.current === true) {
+      return
+    }
+    ;(async () => {
+      onceRef.current = true
+      openMovieDetailModal(movieDetail)
+    })()
+  }, [openMovieDetailModal])
+
+  return null
+}
