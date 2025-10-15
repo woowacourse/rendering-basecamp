b37f3cdf3b4e81ae3d2db6285ee30a982227c7fb
kimyou1102 <yoosk5485@naver.com>
2025-10-16 00:47:38 +0900

    feat: detail 페이지 OG/meta 태그 서버사이드 렌더링 적용

--- nextjs/src/pages/detail/[movieId].tsx
+++ nextjs/src/pages/detail/[movieId].tsx
@@ -1,35 +1,67 @@
-import { useMovieDetailModal } from "../../hooks/useMovieDetailModal";
-import { useEffect, useRef } from "react";
-import { useRouter } from "next/router";
-
+import Head from "next/head";
 import { moviesApi } from "../../api/movies";
 import MovieHomePage from "@/components/MovieHomePage";
+import { useEffect, useRef } from "react";
+import { useMovieDetailModal } from "@/hooks/useMovieDetailModal";
+import { MovieItem } from "@/types/Movie.types";
+import { MovieDetailResponse } from "@/types/MovieDetail.types";
+import { GetServerSidePropsContext } from "next";
 
-export default function MovieDetailPage() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const movieId = Number(context.params!.movieId);
+  const res = await moviesApi.getDetail(movieId);
+
+  const host = context.req.headers.host;
+  const protocol = context.req.headers["x-forwarded-proto"] || "https";
+  const origin = `${protocol}://${host}`;
+
+  return {
+    props: {
+      movie: res.data,
+      origin,
+    },
+  };
+}
+
+interface MovieDetailPage {
+  movie: MovieDetailResponse;
+  origin: string;
+}
+
+export default function MovieDetailPage({ movie, origin }: MovieDetailPage) {
+  console.log("movie", movie);
+  const { title, overview, vote_average, poster_path, id } = movie;
+
+  const imageUrl = poster_path
+    ? `https://image.tmdb.org/t/p/original${poster_path}`
+    : "/images/no_image.png";
+
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={overview} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content={`${origin}/${id}`} />
+        <meta property="og:type" content="website" />
+      </Head>
+
       <MovieHomePage />
-      <DetailPageOpenModal />
+      <DetailPageOpenModal movie={movie} />
     </>
   );
 }
 
-function DetailPageOpenModal() {
-  const router = useRouter();
-  const { movieId } = router.query;
+function DetailPageOpenModal({ movie }: { movie: MovieDetailResponse }) {
   const { openMovieDetailModal } = useMovieDetailModal();
   const onceRef = useRef(false);
 
   useEffect(() => {
-    if (movieId == null || onceRef.current === true) {
-      return;
-    }
-    (async () => {
-      onceRef.current = true;
-      const movieDetail = await moviesApi.getDetail(Number(movieId));
-      openMovieDetailModal(movieDetail.data);
-    })();
-  }, [movieId, openMovieDetailModal]);
+    if (onceRef.current) return;
+    onceRef.current = true;
+    openMovieDetailModal(movie);
+  }, [movie, openMovieDetailModal]);
 
   return null;
 }
