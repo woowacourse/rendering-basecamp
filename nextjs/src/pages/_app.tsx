3ebffdc103a9600ec980c86bf384f5456e11290a
ShinjungOh <tlswjd9260@gmail.com>
2025-10-15 11:26:48 +0900

    feat: _app에 OverlayProvider 적용

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import "@/styles/index.css";
+import {OverlayProvider} from "overlay-kit";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <OverlayProvider>
+      <Component {...pageProps} />
+    </OverlayProvider>
+  );
 }
