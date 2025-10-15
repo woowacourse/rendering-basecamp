317962e484da6d2134b5a5e7b5e58912dbec6296
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-16 00:16:29 +0900

    chore: next 마이그레이션

--- nextjs/src/pages/MovieHomePage.tsx
+++ nextjs/src/pages/MovieHomePage.tsx
@@ -1,8 +1,8 @@
-import { Header } from '../components/Header';
-import { MovieList } from '../components/MovieList';
-import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
+import { Header } from "../components/Header";
+import { MovieList } from "../components/MovieList";
+import { Footer } from "../components/Footer";
+import { usePopularMovies } from "../hooks/queries/usePopularMovies";
+import { Loading } from "../components/common/Loading";
 
 export default function MovieHomePage() {
   const { data: movies, isLoading } = usePopularMovies();
--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -1,8 +1,8 @@
-import { useMovieDetailModal } from '../hooks/useMovieDetailModal';
-import { useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import MovieHomePage from './MovieHomePage';
-import { moviesApi } from '../api/movies';
+import { useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import MovieHomePage from "../MovieHomePage";
+import { useMovieDetailModal } from "@/hooks/useMovieDetailModal";
+import { moviesApi } from "@/api/movies";
 
 export default function MovieDetailPage() {
   return (
@@ -14,14 +14,14 @@ export default function MovieDetailPage() {
 }
 
 function DetailPageOpenModal() {
-  const { movieId } = useParams();
+  const router = useRouter();
+  const { movieId } = router.query;
   const { openMovieDetailModal } = useMovieDetailModal();
   const onceRef = useRef(false);
 
   useEffect(() => {
-    if (movieId == null || onceRef.current === true) {
-      return;
-    }
+    if (movieId == null || onceRef.current) return;
+
     (async () => {
       onceRef.current = true;
       const movieDetail = await moviesApi.getDetail(Number(movieId));
