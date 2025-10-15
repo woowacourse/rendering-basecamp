8b32e1c8d28be942118c1122ff7c69e6c52f42e5
ShinjungOh <tlswjd9260@gmail.com>
2025-10-15 17:53:00 +0900

    refactor: getServerSideProps를 이용해 영화 상세 정보 조회

--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -2,21 +2,27 @@ import Home from '../index';
 import {useEffect, useRef} from "react";
 import {moviesApi} from "@/api/movies";
 import {useMovieDetailModal} from "@/hooks/useMovieDetailModal";
-import {useRouter} from "next/router";
 import {MovieItem} from "@/types/Movie.types";
 import {GetServerSideProps} from "next";
+import {MovieDetailResponse} from "@/types/MovieDetail.types";
 
-interface HomeProps {
+interface DetailProps {
   movies: MovieItem[];
+  detail: MovieDetailResponse | null;
   error: string | null;
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+export const getServerSideProps: GetServerSideProps<DetailProps> = async (context) => {
+  const {id} = context.params as {id: string};
+
   try {
-    const response = await moviesApi.getPopular();
+    const moviesResponse = await moviesApi.getPopular();
+    const movieDetail = await moviesApi.getDetail(Number(id));
+
     return {
       props: {
-        movies: response.data.results,
+        movies: moviesResponse.data.results,
+        detail: movieDetail.data,
         error: null
       }
     };
@@ -25,37 +31,36 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
     return {
       props: {
         movies: [],
+        detail: null,
         error: '영화 정보를 불러오는데 실패했습니다.'
       }
     }
   }
 }
 
-export default function MovieDetailPage({movies, error}: HomeProps) {
+export default function MovieDetailPage({movies, detail, error}: DetailProps) {
   return (
     <>
-      <Home movies={movies} error={error} />
-      <DetailPageOpenModal />
+      <Home movies={movies} error={error}/>
+      <DetailPageOpenModal detail={detail}/>
     </>
   );
 }
 
-function DetailPageOpenModal() {
-  const router = useRouter();
-  const {movieId} = router.query;
+function DetailPageOpenModal({detail}: {
+  detail: MovieDetailResponse | null;
+}) {
   const {openMovieDetailModal} = useMovieDetailModal();
   const onceRef = useRef(false);
 
   useEffect(() => {
-    if (movieId == null || onceRef.current) {
+    if (detail == null || onceRef.current) {
       return;
     }
-    (async () => {
-      onceRef.current = true;
-      const movieDetail = await moviesApi.getDetail(Number(movieId));
-      openMovieDetailModal(movieDetail.data);
-    })();
-  }, [movieId, openMovieDetailModal]);
+
+    onceRef.current = true;
+    openMovieDetailModal(detail);
+  }, [detail, openMovieDetailModal]);
 
   return null;
 }
