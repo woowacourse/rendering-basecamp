891f894b1f3d9e7d1d1ffc2e30231d9df84f8ab5
spoyodevelop <kjy9505@gmail.com>
2025-10-15 14:50:12 +0900

    refactor: 무비 홈페이지 SSR 작성

--- nextjs/src/pages/MovieHomePage.tsx
+++ nextjs/src/pages/MovieHomePage.tsx
@@ -1,16 +1,15 @@
 import { Header } from "../components/Header";
 import { MovieList } from "../components/MovieList";
 import { Footer } from "../components/Footer";
-import { usePopularMovies } from "../hooks/queries/usePopularMovies";
-import { Loading } from "../components/common/Loading";
+import { GetServerSideProps } from "next";
+import { moviesApi } from "../api/movies";
+import { MovieItem } from "../types/Movie.types";
 
-export default function MovieHomePage() {
-  const { data: movies, isLoading } = usePopularMovies();
+export interface MovieHomePageProps {
+  movies: MovieItem[] | null;
+}
 
-  if (isLoading === true) {
-    return <Loading />;
-  }
-
+export default function MovieHomePage({ movies }: MovieHomePageProps) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
@@ -23,3 +22,15 @@ export default function MovieHomePage() {
     </div>
   );
 }
+
+export const fetchMoviesForSSR = async () => {
+  try {
+    const movieDetail = await moviesApi.getPopular();
+    return { props: { movies: movieDetail.data.results } };
+  } catch {
+    return { props: { movies: null } };
+  }
+};
+
+export const getServerSideProps: GetServerSideProps<MovieHomePageProps> =
+  fetchMoviesForSSR;
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import MovieHomePage from "./MovieHomePage";
+import type { MovieHomePageProps } from "./MovieHomePage";
+import { fetchMoviesForSSR } from "./MovieHomePage";
 
-export default function Home() {
+export default function Home({ movies }: MovieHomePageProps) {
   return (
     <>
       <Head>
@@ -10,7 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MovieHomePage />
+      <MovieHomePage movies={movies} />
     </>
   );
 }
+export const getServerSideProps = fetchMoviesForSSR;
