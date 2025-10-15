5d0d94f1d43cfd23d75433120c49b4f276c6ae89
mun-kyeong <horse6953@naver.com>
2025-10-16 02:10:49 +0900

    feat: background-img 스타일을 image 태그로 변경

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
