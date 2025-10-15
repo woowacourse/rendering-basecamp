9ba3dc93b9db6759186192945833df1700054390
AHHYUNJU <wndkgus0225@naver.com>
2025-10-16 01:37:33 +0900

    fix: img 태그를 Image 태그로 변경

--- nextjs/src/components/Footer.tsx
+++ nextjs/src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+
 export const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 우아한테크코스 All Rights Reserved.</p>
       <p>
-        <img
+        <Image
           src="/images/woowacourse_logo.png"
           width="180"
           alt="우아한테크코스"
