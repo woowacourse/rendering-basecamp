406d6caaf529c7d469528d50d3c045384ea145ea
jeongyou <wjddbwjd1205@naver.com>
2025-10-15 19:57:52 +0900

    chore: react-csr 코드 가져오기

---
+++ nextjs/src/hooks/useMovieDetailModal.tsx
@@ -0,0 +1,21 @@
+import { overlay } from 'overlay-kit';
+import { MovieDetailModal } from '../components/MovieDetailModal';
+import { MovieDetailResponse } from '../types/MovieDetail.types';
+
+export const useMovieDetailModal = () => {
+  const openMovieDetailModal = (movie: MovieDetailResponse) => {
+    return new Promise<void>((resolve) => {
+      overlay.open(({ unmount }) => (
+        <MovieDetailModal
+          movie={movie}
+          onClose={() => {
+            resolve();
+            unmount();
+          }}
+        />
+      ));
+    });
+  };
+
+  return { openMovieDetailModal };
+};
