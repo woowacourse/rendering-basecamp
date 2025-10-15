a01ccc7e1b24cfc674aa807ccd41095ec3186b65
mlnwns <mlnwns@naver.com>
2025-10-15 20:20:44 +0900

    feat: OverlayProvider 추가 및 메타데이터 수정

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { OverlayProvider } from "overlay-kit";
 import "../styles/index.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <OverlayProvider>
+      <Component {...pageProps} />
+    </OverlayProvider>
+  );
 }
