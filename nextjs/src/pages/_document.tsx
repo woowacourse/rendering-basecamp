b0fa8b1172406242a82798fa1401c89ea7fe8678
minji2219 <aabb0219@naver.com>
2025-10-16 01:17:22 +0900

    chore: title 수정

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>영화 소개</title>
-      <meta name="description" content="영화 소개를 위한 사이트" />
+      <meta name="description" content="영화 소개를 위한 서비스" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <body>
