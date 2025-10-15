2b1a846d6ed10d911a504e432bae7e9f787c6194
deokbin <deokbin@naver.com>
2025-10-15 16:38:14 +0900

    feat: props 수정

--- nextjs/src/components/MovieItem.tsx
+++ nextjs/src/components/MovieItem.tsx
@@ -1,41 +1,29 @@
-import type { MovieItem as MovieItemType } from '../types/Movie.types';
+import { MovieItemType } from "@/types/Movie.types";
 
 interface MovieItemProps {
   movie: MovieItemType;
-  onClick: (movie: MovieItemType) => void;
   ref?: React.Ref<HTMLLIElement>;
 }
 
-export const MovieItem = ({ movie, onClick, ref }: MovieItemProps) => {
+export const MovieItem = ({ movie }: MovieItemProps) => {
   const { title, poster_path, vote_average } = movie;
 
   const imageUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
-    : '/images/no_image.png';
-
-  const handleClick = () => {
-    onClick(movie);
-  };
+    : "/images/no_image.png";
 
   return (
-    <li
-      ref={ref}
-      className="movie-item"
-      onClick={handleClick}
-      data-index={movie.id}
-    >
-      <div className="item">
-        <img className="thumbnail" src={imageUrl} alt={title} loading="lazy" />
-        <div className="item-desc">
-          <p className="rate">
-            <img src="/images/star_empty.png" className="star" />
-            <span>{vote_average.toFixed(1)}</span>
-          </p>
-          <strong>{title}</strong>
-        </div>
+    <div className="item">
+      <img className="thumbnail" src={imageUrl} alt={title} loading="lazy" />
+      <div className="item-desc">
+        <p className="rate">
+          <img src="/images/star_empty.png" className="star" />
+          <span>{vote_average.toFixed(1)}</span>
+        </p>
+        <strong>{title}</strong>
       </div>
-    </li>
+    </div>
   );
 };
 
-MovieItem.displayName = 'MovieItem';
+MovieItem.displayName = "MovieItem";
--- nextjs/src/components/MovieList.tsx
+++ nextjs/src/components/MovieList.tsx
@@ -1,27 +1,19 @@
-import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import Link from "next/link";
 import { MovieItem } from "./MovieItem";
-import type { MovieItem as MovieItemType } from "../types/Movie.types";
-import { moviesApi } from "../api/movies";
+import type { MovieItemType } from "@/types/Movie.types";
 
 export const MovieList = ({ movies }: { movies: MovieItemType[] }) => {
-  const { openMovieDetailModal } = useMovieDetailModal();
-
-  const handleMovieClick = async (movie: MovieItemType) => {
-    const movieDetail = await moviesApi.getDetail(movie.id);
-    await openMovieDetailModal(movieDetail.data);
-  };
-
   return (
     <main>
       <section className="container">
         <h2 className="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
         <ul className="thumbnail-list">
           {movies.map((movie) => (
-            <MovieItem
-              key={movie.id}
-              movie={movie}
-              onClick={handleMovieClick}
-            />
+            <li className="movie-item" data-index={movie.id} key={movie.id}>
+              <Link href={`/detail/${movie.id}`}>
+                <MovieItem movie={movie} />
+              </Link>
+            </li>
           ))}
         </ul>
       </section>
