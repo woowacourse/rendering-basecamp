17a89993f403286e34f2e5572de09a6701dd19fb
mlnwns <mlnwns@naver.com>
2025-10-15 20:14:45 +0900

    refactor: react-csr 코드 nextjs로 마이그레이션

---
+++ nextjs/src/components/FeaturedMovie.tsx
@@ -0,0 +1,32 @@
+import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import { Button } from "./common/Button";
+import type { MovieItem } from "../types/Movie.types";
+import { moviesApi } from "../api/movies";
+
+interface FeaturedMovieProps {
+  movie: MovieItem;
+}
+
+export const FeaturedMovie = ({ movie }: FeaturedMovieProps) => {
+  const { openMovieDetailModal } = useMovieDetailModal();
+
+  const handleDetailClick = async () => {
+    const movieDetail = await moviesApi.getDetail(movie.id);
+    await openMovieDetailModal(movieDetail.data);
+  };
+
+  return (
+    <div className="top-rated-movie">
+      <div className="rate">
+        <img src="/images/star_empty.png" width="32" height="32" />
+        <span className="text-2xl font-semibold text-yellow">
+          {movie.vote_average}
+        </span>
+      </div>
+      <h1 className="text-3xl font-semibold">{movie.title}</h1>
+      <Button variant="primary" onClick={handleDetailClick} className="detail">
+        자세히 보기
+      </Button>
+    </div>
+  );
+};
---
+++ nextjs/src/pages/detail/[id]/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+import { useMovieDetailModal } from "@/hooks/useMovieDetailModal";
+import MovieHomePage from "@/pages";
+import { useParams } from "next/navigation";
+import { moviesApi } from "@/api/movies";
+
+export default function MovieDetailPage() {
+  return (
+    <>
+      <MovieHomePage />
+      <DetailPageOpenModal />
+    </>
+  );
+}
+
+function DetailPageOpenModal() {
+  const { movieId } = useParams();
+  const { openMovieDetailModal } = useMovieDetailModal();
+  const onceRef = useRef(false);
+
+  useEffect(() => {
+    if (movieId == null || onceRef.current === true) {
+      return;
+    }
+    (async () => {
+      onceRef.current = true;
+      const movieDetail = await moviesApi.getDetail(Number(movieId));
+      openMovieDetailModal(movieDetail.data);
+    })();
+  }, [movieId, openMovieDetailModal]);
+
+  return null;
+}
