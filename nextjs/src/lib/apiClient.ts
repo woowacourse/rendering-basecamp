241ac94625f5e7a164609da0d35faffedd09f0ac
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-15 20:00:50 +0900

    fix: apiClient 환경 변수 수정

--- nextjs/src/lib/apiClient.ts
+++ nextjs/src/lib/apiClient.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios"
 
 export const apiClient = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
   },
-});
+})
