661e4e9f5c306791ca5db4f2ac2fcc3a90b8f82a
mun-kyeong <horse6953@naver.com>
2025-10-16 03:58:11 +0900

    feat: Open Graph 및 Twitter 메타 태그 추가 및 정리

--- nextjs/src/components/SeoHead.tsx
+++ nextjs/src/components/SeoHead.tsx
@@ -21,16 +21,23 @@ export const SeoHead = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
 
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={`${title} 포스터`} />
       <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="My Movie App" />
+      <meta property="og:locale" content="ko_KR" />
 
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
+      <meta name="twitter:image:alt" content={`${title} 포스터`} />
+      <meta name="twitter:site" content="@my_movie_app" />
     </Head>
   );
 };
