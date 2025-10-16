aa8abbc4414a94b69ac9a076e937a5fdc6e3f69a
yeongipark <yeongi0111@naver.com>
2025-10-16 15:07:52 +0900

    chore: 메인 화면 og 추가

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Footer } from "../components/Footer";
 import { moviesApi } from "../api/movies";
 import type { MovieItem } from "../types/Movie.types";
 import type { GetServerSideProps } from "next";
+import Head from "next/head";
 
 interface MovieHomePageProps {
   movies: MovieItem[];
@@ -13,13 +14,30 @@ export default function MovieHomePage({ movies }: MovieHomePageProps) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
+  const ogContent = "보고싶은 영화의 리뷰를 확인하세요";
+  const ogImage = `https://image.tmdb.org/t/p/w1280${movies[0].poster_path}`;
 
   return (
-    <div id="wrap">
-      <Header featuredMovie={movies[0]} />
-      <MovieList movies={movies} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title></title>
+        <meta name="description" content={ogContent} />
+
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:title" content="영화 리뷰" />
+        <meta property="og:description" content={ogContent} />
+        <meta property="og:image" content={ogImage} />
+        <meta
+          property="og:url"
+          content={"https://nextjs-nine-beta-15.vercel.app/"}
+        />
+      </Head>
+      <div id="wrap">
+        <Header featuredMovie={movies[0]} />
+        <MovieList movies={movies} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
