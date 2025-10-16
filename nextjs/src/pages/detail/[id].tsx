4167e46339365a660086babd255fdd4e81801493
hanheel <hanhee630630@gmail.com>
2025-10-16 18:23:36 +0900

    feat: 메타태그 추가

--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -3,6 +3,7 @@ import { moviesApi } from "@/api/movies";
 import MovieHomePage from "@/pages/movies";
 import type { MovieDetailResponse } from "@/types/MovieDetail.types";
 import type { MovieItem } from "@/types/Movie.types";
+import Head from "next/head";
 
 interface MovieDetailPageProps {
 	movie: MovieDetailResponse;
@@ -14,7 +15,44 @@ export default function MovieDetailPage({ movie }: MovieDetailPageProps) {
 		...movie,
 		genre_ids: genreIds,
 	};
-	return <MovieHomePage movies={[movieItem]} />;
+	return (
+		<>
+			<Head>
+				<title>{movie.title}</title>
+				<meta
+					name="description"
+					content={movie.overview || "줄거리 정보가 없습니다."}
+				/>
+
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={movie.title} />
+				<meta
+					property="og:description"
+					content={movie.overview || "줄거리 정보가 없습니다."}
+				/>
+				<meta
+					property="og:image"
+					content={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+				/>
+				<meta
+					property="og:url"
+					content={`https://rendering-basecamp-gold.vercel.app/movies/${movie.id}`}
+				/>
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content={movie.title} />
+				<meta
+					name="twitter:description"
+					content={movie.overview || "줄거리 정보가 없습니다."}
+				/>
+				<meta
+					name="twitter:image"
+					content={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+				/>
+			</Head>
+			<MovieHomePage movies={[movieItem]} />
+		</>
+	);
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
