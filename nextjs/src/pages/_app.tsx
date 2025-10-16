a07a009689ebe2e31be24ea18799ee5d4f4e598d
haku <hakukudh@gmail.com>
2025-10-16 09:50:21 +0900

    style: 스타일 파일 복사

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,5 +1,19 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { OverlayProvider } from 'overlay-kit';
+import '../styles/reset.css';
+import '../styles/colors.css';
+import '../styles/text.css';
+import '../styles/main.css';
+import '../styles/thumbnail.css';
+import '../styles/modal.css';
+import '../styles/animation.css';
+import '../styles/media.css';
+import '../styles/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <OverlayProvider>
+      <Component {...pageProps} />
+    </OverlayProvider>
+  );
 }
