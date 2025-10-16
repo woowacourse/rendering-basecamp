e6aca186def05db13848d6f80e53dd0913359add
haku <hakukudh@gmail.com>
2025-10-16 11:43:57 +0900

    feat: open graph tag 적용

--- nextjs/src/pages/detail/[movieId].tsx
+++ nextjs/src/pages/detail/[movieId].tsx
@@ -23,11 +23,35 @@ export default function MovieDetailPage({ movies, movieDetail }: MovieDetailPage
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
+  const posterUrl = movieDetail.poster_path
+    ? `https://image.tmdb.org/t/p/original${movieDetail.poster_path}`
+    : '/images/no_image.png';
+
+  const backdropUrl = movieDetail.backdrop_path
+    ? `https://image.tmdb.org/t/p/original${movieDetail.backdrop_path}`
+    : posterUrl;
   return (
     <>
       <Head>
-        <title>Movie Detail</title>
+        <title>{movieDetail.title} - Movie Review</title>
+        <meta name="description" content={movieDetail.overview || `${movieDetail.title} 영화 정보`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:title" content={`${movieDetail.title} - Movie Review`} />
+        <meta property="og:description" content={movieDetail.overview || `${movieDetail.title} 영화 정보`} />
+        <meta property="og:image" content={backdropUrl} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:url"
+          content={`https://rendering-basecamp-pink-seven.vercel.app//detail/${movieDetail.id}`}
+        />
+        <meta property="og:site_name" content="Movie Review App" />
+
+        <meta property="video:release_date" content={movieDetail.release_date} />
+        {movieDetail.genres &&
+          movieDetail.genres.map((genre) => <meta key={genre.id} property="video:tag" content={genre.name} />)}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div id="wrap">
