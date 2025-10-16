7a8fd28bcb5f969fa6e5823ff1a6a3cf14445d62
keemsebeen <sebin0580@gmail.com>
2025-10-16 14:16:04 +0900

    feat: Home meta 태그 추가

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { MovieList } from "@/components/MovieList";
 import { MovieItem } from "@/types/Movie.types";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Head from "next/head";
 
 export const getServerSideProps = (async () => {
   const moviesRes = await moviesApi.getPopular();
@@ -19,6 +20,20 @@ export default function Home({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
+      <Head>
+        <title>세라의 영화 앱</title>
+        <meta property="og:title" content="세라의 영화 앱" key="title" />
+        <meta
+          property="og:description"
+          content="지금 인기 있는 영화를 확인하세요"
+          key="description"
+        />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_APP_URL}
+          key="url"
+        />
+      </Head>
       <div id="wrap">
         <Header featuredMovie={movies[0]} />
         <MovieList movies={movies} />
