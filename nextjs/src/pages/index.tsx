fa27ee902bde7ca50b928a98180671cda78ea226
dev-dino22 <wlsskdud02@naver.com>
2025-10-16 10:58:27 +0900

    refactor: Home 페이지에서 MainContent 분리

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -2,14 +2,14 @@ import { moviesApi } from "@/api/movies";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { MovieList } from "@/components/MovieList";
+import { OGMetaTags } from "@/components/OGMetaTags";
 import { MovieItem } from "@/types/Movie.types";
 import { GetServerSideProps } from "next";
 
-function Home({ movies }: { movies: MovieItem[] }) {
+export function MainContent({ movies }: { movies: MovieItem[] }) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
-
   return (
     <div id="wrap">
       <Header featuredMovie={movies[0]} />
@@ -19,14 +19,27 @@ function Home({ movies }: { movies: MovieItem[] }) {
   );
 }
 
+function Home({ movies }: { movies: MovieItem[] }) {
+  return (
+    <>
+      <OGMetaTags
+        title="영화 리뷰"
+        description="오늘 뭐 볼까? 지금 인기 있는 영화를 확인해보세요!"
+        url="/"
+      />
+      <MainContent movies={movies} />
+    </>
+  );
+}
+
 export default Home;
 
 export const getServerSideProps: GetServerSideProps<{
   movies: MovieItem[];
 }> = async () => {
   try {
-    const movieDetail = await moviesApi.getPopular();
-    const movies: MovieItem[] = movieDetail.data.results;
+    const moviesData = await moviesApi.getPopular();
+    const movies: MovieItem[] = moviesData.data.results;
     return { props: { movies } };
   } catch (error) {
     console.error("영화 데이터를 불러오는 데 실패했습니다.", error);
