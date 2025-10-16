e04ca719660378cefab211a08fc27851a2e9c75c
hanheel <hanhee630630@gmail.com>
2025-10-16 18:15:11 +0900

    fix: 빌드 오류 해결

--- nextjs/src/pages/movies/index.tsx
+++ nextjs/src/pages/movies/index.tsx
@@ -2,17 +2,15 @@ import { Header } from "@/components/Header";
 import { MovieList } from "@/components/MovieList";
 import { Footer } from "@/components/Footer";
 import type { MovieItem } from "@/types/Movie.types";
-import type { GetServerSideProps } from "next";
-import { moviesApi } from "@/api/movies";
 
 interface MovieHomePageProps {
 	movies: MovieItem[];
 }
 
-export default function MovieHomePage({ movies }: MovieHomePageProps) {
+export default function MovieHomePage({ movies = [] }: MovieHomePageProps) {
 	return (
 		<div id="wrap">
-			<Header featuredMovie={movies[0]} />
+			<Header featuredMovie={movies[0] || null} />
 			<MovieList movies={movies} />
 			<Footer />
 		</div>
