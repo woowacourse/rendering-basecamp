498738ca14cf2fa7e2afe9d832710bcbcc717137
eunwoo-levi <eunwoo1341@gmail.com>
2025-10-16 10:32:26 +0900

    feat: movie detail 페이지에 Metadata 추가

--- nextjs/src/pages/detail/[movieId].tsx
+++ nextjs/src/pages/detail/[movieId].tsx
@@ -1,7 +1,9 @@
+import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { useMovieDetailModal } from '../../hooks/useMovieDetailModal';
 import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
+import { Header } from '../../components/Header';
 import { MovieList } from '../../components/MovieList';
 import { Footer } from '../../components/Footer';
 import { moviesApi } from '../../api/movies';
@@ -14,11 +16,55 @@ interface MovieDetailPageProps {
 }
 
 export default function MovieDetailPage({ movie, movies }: MovieDetailPageProps) {
+    const featuredMovie = movies[0];
+
+    const ogImage = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : '/images/logo.png';
+
+    const description = movie.overview || `${movie.title} - 영화 상세 정보`;
+    const genres = movie.genres.map(g => g.name).join(', ');
 
     return (
         <>
+            <Head>
+                <title>{movie.title} - 영화 상세 정보</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={`${movie.title}, 영화, ${genres}`} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+                <meta property="og:title" content={`${movie.title} - 영화 상세 정보`} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={ogImage} />
+                <meta property="og:type" content="video.movie" />
+                <meta property="og:locale" content="ko_KR" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={`${movie.title} - 영화 상세 정보`} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:image" content={ogImage} />
+
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@type": "Movie",
+                            "name": movie.title,
+                            "description": description,
+                            "image": ogImage,
+                            "genre": movie.genres.map(g => g.name),
+                            "aggregateRating": {
+                                "@type": "AggregateRating",
+                                "ratingValue": movie.vote_average,
+                                "ratingCount": movie.vote_count,
+                            },
+                        }),
+                    }}
+                />
+            </Head>
             <div id="wrap">
+                <Header featuredMovie={featuredMovie} />
                 <MovieList movies={movies} />
                 <Footer />
             </div>
