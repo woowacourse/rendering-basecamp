f9d54de5431b7220f217be5ae4aa2ffd44b1d270
keemsebeen <sebin0580@gmail.com>
2025-10-16 10:51:40 +0900

    chore: 기존 component, hook, style, type 이전

---
+++ nextjs/src/pages/detail/[id]/index.tsx
@@ -0,0 +1,32 @@
+import { useMovieDetailModal } from "../../../hooks/useMovieDetailModal";
+import { useEffect, useRef } from "react";
+
+import { moviesApi } from "../../../api/movies";
+import { useParams } from "next/navigation";
+
+export default function MovieDetailPage() {
+  return (
+    <>
+      <DetailPageOpenModal />
+    </>
+  );
+}
+
+function DetailPageOpenModal() {
+  const { movieId } = useParams();
+  const { openMovieDetailModal } = useMovieDetailModal();
+  const onceRef = useRef(false);
+
+  useEffect(() => {
+    if (movieId == null || onceRef.current === true) {
+      return;
+    }
+    (async () => {
+      onceRef.current = true;
+      const movieDetail = await moviesApi.getDetail(Number(movieId));
+      openMovieDetailModal(movieDetail.data);
+    })();
+  }, [movieId, openMovieDetailModal]);
+
+  return null;
+}
