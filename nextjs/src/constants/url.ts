0a4c16b58d2700af5fd7aec7ee261300c3b0d02c
ohgus <anakin1108@naver.com>
2025-10-16 11:03:58 +0900

    feat: 메인 페이지 SSR 구현

---
+++ nextjs/src/constants/url.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://rendering-basecamp-ohgus.vercel.app/';
+export const HOME_URL = `${BASE_URL}/`;
+export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/';
+export const TMDB_NO_IMAGE_URL = '/images/no_image.png';
