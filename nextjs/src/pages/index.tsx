b78d44badd117a979089b8043bb5cc40762e3e7e
haku <hakukudh@gmail.com>
2025-10-16 11:11:23 +0900

    feat: getServerSideProps 적용

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,17 +1,16 @@
 import { Header } from '../components/Header';
 import { MovieList } from '../components/MovieList';
 import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
 import Head from 'next/head';
+import { MovieItem } from '../types/Movie.types';
+import { moviesApi } from '@/api/movies';
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
-  const { data: movies, isLoading } = usePopularMovies();
+interface HomeProps {
+  movies: MovieItem[];
+}
 
-  if (isLoading === true) {
-    return <Loading />;
-  }
-
+export default function Home({ movies }: HomeProps) {
   if (movies == null || movies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
@@ -22,6 +21,9 @@ export default function Home() {
         <title>Movie Review App</title>
         <meta name="description" content="영화 리뷰 앱" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Movie Review App" />
+        <meta property="og:description" content="인기 영화 목록을 확인하고 리뷰를 남겨보세요" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div id="wrap">
@@ -32,3 +34,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  try {
+    const response = await moviesApi.getPopular();
+
+    return {
+      props: {
+        movies: response.data.results,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        movies: [],
+      },
+    };
+  }
+};
