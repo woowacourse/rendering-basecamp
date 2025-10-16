42377e1f7a3cebfd405585a4fd8a55f70a2f2970
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-16 11:34:04 +0900

    chore: 메타 데이터 추가

--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -1,24 +1,60 @@
-import { GetServerSideProps } from "next";
+import Head from "next/head";
 import { useEffect, useRef } from "react";
-import { moviesApi } from "@/api/movies";
+import { Header } from "@/components/Header";
+import { MovieList } from "@/components/MovieList";
+import { Footer } from "@/components/Footer";
 import { useMovieDetailModal } from "@/hooks/useMovieDetailModal";
-import MovieHomePage from "../MovieHomePage";
 import { MovieItem } from "@/types/Movie.types";
 import { MovieDetailResponse } from "@/types/MovieDetail.types";
 
 type DetailPageProps = {
   popularMovies: MovieItem[];
   movieDetail: MovieDetailResponse | null;
+  siteUrl: string;
 };
 
 export default function MovieDetailPage({
   popularMovies,
   movieDetail,
+  siteUrl,
 }: DetailPageProps) {
+  const title = movieDetail ? `${movieDetail.title} | 인기 영화` : "영화 상세";
+  const description = movieDetail?.overview
+    ? movieDetail.overview.slice(0, 100)
+    : "영화 상세 정보를 확인해보세요!";
+  const image = movieDetail?.poster_path
+    ? `https://image.tmdb.org/t/p/w780${movieDetail.poster_path}`
+    : `${siteUrl}/images/og-image.jpg`;
+  const url = `${siteUrl}/detail/${movieDetail?.id ?? ""}`;
+
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <DetailPageOpenModal movieDetail={movieDetail} />
-      <MovieHomePage initialMovies={popularMovies} />
+      <div id="wrap">
+        <Header featuredMovie={popularMovies[0]} />
+        <MovieList movies={popularMovies} />
+        <Footer />
+      </div>
     </>
   );
 }
@@ -39,30 +75,3 @@ function DetailPageOpenModal({
 
   return null;
 }
-
-export const getServerSideProps: GetServerSideProps<DetailPageProps> = async (
-  context
-) => {
-  const { id } = context.params!;
-
-  try {
-    const [popularRes, detailRes] = await Promise.all([
-      moviesApi.getPopular(),
-      moviesApi.getDetail(Number(id)),
-    ]);
-
-    return {
-      props: {
-        popularMovies: popularRes.data.results,
-        movieDetail: detailRes.data,
-      },
-    };
-  } catch (e) {
-    return {
-      props: {
-        popularMovies: [],
-        movieDetail: null,
-      },
-    };
-  }
-};
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,28 +1,77 @@
-import MovieHomePage from "./MovieHomePage";
+import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { moviesApi } from "@/api/movies";
 import { MovieItem } from "@/types/Movie.types";
+import { Header } from "@/components/Header";
+import { MovieList } from "@/components/MovieList";
+import { Footer } from "@/components/Footer";
 
 type HomeProps = {
   popularMovies: MovieItem[];
+  siteUrl: string;
 };
 
-export default function Home({ popularMovies }: HomeProps) {
-  return <MovieHomePage initialMovies={popularMovies} />;
+export default function Home({ popularMovies, siteUrl }: HomeProps) {
+  const title = "지금 인기 있는 영화";
+  const description = "지금 가장 인기 있는 영화를 만나보세요!";
+  const image = `${siteUrl}/images/og-image.jpg`;
+
+  if (!popularMovies || popularMovies.length === 0) {
+    return <div>영화 정보를 불러오는데 실패했습니다.</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div id="wrap">
+        <Header featuredMovie={popularMovies[0]} />
+        <MovieList movies={popularMovies} />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+export const getServerSideProps: GetServerSideProps<HomeProps> = async (
+  context
+) => {
+  const host = context.req.headers.host;
+  const protocol =
+    (context.req.headers["x-forwarded-proto"] as string) || "http";
+  const siteUrl = `${protocol}://${host}`;
+
   try {
     const res = await moviesApi.getPopular();
     return {
       props: {
         popularMovies: res.data.results,
+        siteUrl,
       },
     };
-  } catch (e) {
+  } catch {
     return {
       props: {
         popularMovies: [],
+        siteUrl,
       },
     };
   }
