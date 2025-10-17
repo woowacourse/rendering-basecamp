a15e54e626ecb64fcdf0ea266fd956af74389af7
hoyyChoi <hoyeon8165@gmail.com>
2025-10-17 14:23:42 +0900

    fix: div 태그 -> Link 컴포넌트로 변경

--- nextjs/src/components/MovieItem.tsx
+++ nextjs/src/components/MovieItem.tsx
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import type { MovieItem as MovieItemType } from "../types/Movie.types";
 
 interface MovieItemProps {
   movie: MovieItemType;
-  onClick: () => void;
   ref?: React.Ref<HTMLLIElement>;
 }
 
-export const MovieItem = ({ movie, onClick, ref }: MovieItemProps) => {
+export const MovieItem = ({ movie, ref }: MovieItemProps) => {
   const { title, poster_path, vote_average } = movie;
 
   const imageUrl = poster_path
@@ -14,22 +14,22 @@ export const MovieItem = ({ movie, onClick, ref }: MovieItemProps) => {
     : "/images/no_image.png";
 
   return (
-    <li
-      ref={ref}
-      className='movie-item'
-      onClick={onClick}
-      data-index={movie.id}
-    >
-      <div className='item'>
+    <li ref={ref} className='movie-item' data-index={movie.id}>
+      <Link
+        href={`/detail/${movie.id}`}
+        className='item'
+        scroll={false}
+        shallow={true}
+      >
         <img className='thumbnail' src={imageUrl} alt={title} loading='lazy' />
         <div className='item-desc'>
           <p className='rate'>
-            <img src='/images/star_empty.png' className='star' />
+            <img src='/images/star_empty.png' className='star' alt='별점' />
             <span>{vote_average.toFixed(1)}</span>
           </p>
           <strong>{title}</strong>
         </div>
-      </div>
+      </Link>
     </li>
   );
 };
