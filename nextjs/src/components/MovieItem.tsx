e35836dfab5606ca29425aad74782f8378bd3219
mlnwns <mlnwns@naver.com>
2025-10-17 13:55:31 +0900

    feat: MovieItem 컴포넌트에서 onClick 핸들러 제거 및 Link 추가

--- nextjs/src/components/MovieItem.tsx
+++ nextjs/src/components/MovieItem.tsx
@@ -1,53 +1,46 @@
 import Image from "next/image";
 import type { MovieItem as MovieItemType } from "../types/Movie.types";
+import Link from "next/link";
 
 interface MovieItemProps {
   movie: MovieItemType;
-  onClick: (movie: MovieItemType) => void;
   ref?: React.Ref<HTMLLIElement>;
 }
 
-export const MovieItem = ({ movie, onClick, ref }: MovieItemProps) => {
+export const MovieItem = ({ movie, ref }: MovieItemProps) => {
   const { title, poster_path, vote_average } = movie;
 
   const imageUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
     : "/images/no_image.png";
 
-  const handleClick = () => {
-    onClick(movie);
-  };
-
   return (
-    <li
-      ref={ref}
-      className="movie-item"
-      onClick={handleClick}
-      data-index={movie.id}
-    >
-      <div className="item">
-        <Image
-          className="thumbnail"
-          src={imageUrl}
-          alt={title}
-          width={200}
-          height={300}
-          loading="lazy"
-        />
-        <div className="item-desc">
-          <p className="rate">
-            <Image
-              alt=""
-              src="/images/star_empty.png"
-              className="star"
-              width={16}
-              height={16}
-            />
-            <span>{vote_average.toFixed(1)}</span>
-          </p>
-          <strong>{title}</strong>
+    <li className="movie-item" data-index={movie.id}>
+      <Link href={`/detail/${movie.id}`}>
+        <div className="item">
+          <Image
+            className="thumbnail"
+            src={imageUrl}
+            alt={title}
+            width={200}
+            height={300}
+            loading="lazy"
+          />
+          <div className="item-desc">
+            <p className="rate">
+              <Image
+                alt=""
+                src="/images/star_empty.png"
+                className="star"
+                width={16}
+                height={16}
+              />
+              <span>{vote_average.toFixed(1)}</span>
+            </p>
+            <strong>{title}</strong>
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 };
