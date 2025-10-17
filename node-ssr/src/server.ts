f3bd62952d7c746e8f946f8cf54b4a12a213b2d7
Minje Sung <minje813@dgu.ac.kr>
2025-10-17 17:47:54 +0900

    style: prettier 적용

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -1,15 +1,15 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Request, Response } from "express";
-import path from "path";
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/", async (_req: Request, res: Response) => {
+app.get('/', async (_req: Request, res: Response) => {
   res.send(/*html*/ `
     <!DOCTYPE html>
     <html lang="ko">
@@ -24,7 +24,7 @@ app.get("/", async (_req: Request, res: Response) => {
 });
 
 // public 폴더 속 정적 파일을 웹에서 접근할 수 있도록 만든다.
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, (): void => {
   console.log(`🌟 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
