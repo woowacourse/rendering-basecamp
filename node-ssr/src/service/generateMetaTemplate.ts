8c966c3a8e3635ee7f1752defd08080d96a9964f
wo-o29 <woogur29@gmail.com>
2025-10-17 14:01:11 +0900

    feat: generateMetaTemplate 유틸 함수 추가

---
+++ node-ssr/src/service/generateMetaTemplate.ts
@@ -0,0 +1,62 @@
+interface Image {
+  url: string;
+  alt: string;
+}
+
+interface GetMetaTemplate {
+  title?: string;
+  description?: string;
+  image?: Image;
+  currentUrl: string;
+}
+
+const defaultTitle = "Movielist";
+const defaultDescription = "모든 영화를 한눈에, Movielist";
+
+export const generateMetaTemplate = ({
+  title,
+  description,
+  image,
+  currentUrl,
+}: GetMetaTemplate) => {
+  const metaTitle = title ? `${title} | ${defaultTitle}` : defaultTitle;
+  const metaDescription = description ? description : defaultDescription;
+  const metaImage = image
+    ? image
+    : {
+        url: "https://rendering-basecamp-wo-o29.vercel.app/images/logo.png",
+        alt: "Movielist 로고",
+      };
+
+  return `
+<title>${metaTitle}</title>
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  <link rel="canonical" href=${currentUrl} />
+
+  <meta name="title" content=${metaTitle} />
+  <meta
+    name="keyword"
+    content="Movielist, 영화 목록, 영화 보기, 인기 영화"
+  />
+  <meta name="description" content=${metaDescription} />
+
+  {/* Open Graph */}
+  <meta property="og:title" content=${metaTitle} />
+  <meta property="og:description" content=${metaDescription} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content=${currentUrl} />
+  <meta property="og:image" content=${metaImage.url} />
+  <meta property="og:image:alt" content=${metaImage.alt} />
+  <meta property="og:locale" content="ko_KR" />
+  <meta property="og:site_name" content="Movielist" />
+
+  {/* Twitter */}
+  <meta property="twitter:title" content=${metaTitle} />
+  <meta name="twitter:description" content=${metaDescription} />
+  <meta name="twitter:card" content="website" />
+  <meta property="twitter:site" content=${currentUrl} />
+  <meta name="twitter:image" content=${metaImage.url} />
+  <meta property="twitter:image:alt" content=${metaImage.alt} />
+`;
+};
