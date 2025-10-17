586fd29100f5eba1677eec04662de6d7ee8ea20a
Minje Sung <minje813@dgu.ac.kr>
2025-10-17 14:23:54 +0900

    refactor: 정적 메타태그 document로 이동

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -11,9 +11,6 @@ export default function Home({ movies }: InferGetServerSidePropsType<typeof getS
     <>
       <Head>
         <title>SSR로 버무린 영화 리뷰</title>
-        <meta name="description" content="최신 영화 목록을 확인해보세요!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div id="wrap">
         <Header featuredMovie={movies[0]} />
