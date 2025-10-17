7121f2f9b807d47f899aedd16a68618beb9e61c2
hanheel <hanhee630630@gmail.com>
2025-10-17 16:42:51 +0900

    chore: 브랜치 이동

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -47,9 +47,43 @@ app.get("/", async (_req: Request, res: Response) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles/index.css" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles/index.css" />
         <title>영화 리뷰</title>
       </head>
       <body>
+        <div id="wrap">
+          <header>
+            <div class="background-container"
+              style="background-image: url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${data.results[0].backdrop_path});">
+              <div class="overlay"></div>
+              <div class="top-rated-container">
+                <img src="/images/logo.png" width="117" height="20" class="logo" alt="MovieLogo" />
+                <div class="top-rated-movie">
+                  <div class="rate">
+                    <img src="/images/star_empty.png" width="32" height="32" />
+                    <span class="text-2xl font-semibold text-yellow">${data.results[0].vote_average.toFixed(1)}</span>
+                  </div>
+                  <h1 class="text-3xl font-semibold">${data.results[0].title}</h1>
+                  <a href="/movie/${data.results[0].id}" class="primary detail">자세히 보기</a>
+                </div>
+              </div>
+            </div>
+          </header>
+          <main>
+            <section class="container">
+              <h2 class="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
+              <ul class="thumbnail-list">
+                ${movieListHTML}
+              </ul>
+            </section>
+          </main>
+          <footer class="footer">
+            <p>&copy; 우아한테크코스 All Rights Reserved.</p>
+            <p><img src="/images/woowacourse_logo.png" width="180" alt="우아한테크코스" /></p>
+          </footer>
+        </div>
         <div id="wrap">
           <header>
             <div class="background-container"
@@ -142,6 +176,7 @@ app.get("/movie/:id", async (req: Request, res: Response) => {
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, (): void => {
+	console.log(`🌟 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 	console.log(`🌟 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
 
