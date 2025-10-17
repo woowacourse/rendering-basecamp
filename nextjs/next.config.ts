9defc09e5dfbacb2f045364a018f2798150d1944
mlnwns <mlnwns@naver.com>
2025-10-17 13:45:03 +0900

    feat: 이미지 도메인 설정을 remotePatterns로 변경

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
