58d4935b4c9836c0d2c9202362888582acfa7f27
keemsebeen <sebin0580@gmail.com>
2025-10-19 02:48:08 +0900

    feat: csr 구현

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import express, { Request, Response } from "express";
 import path from "path";
+import { moviesApi } from "./service/tmdbApi";
+// import { renderHomePage, renderDetailPage } from "./renderer/htmlRenderer";
 
 const app = express();
 const PORT = 8080;
@@ -10,17 +12,26 @@ const PORT = 8080;
 app.use(express.json());
 
 app.get("/", async (_req: Request, res: Response) => {
-  res.send(/*html*/ `
-    <!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <title>영화 리뷰</title>
-      </head>
-      <body>
-        테스트
-      </body>
-    </html>
-        `);
+  try {
+    const movieData = await moviesApi.getPopular();
+    res.json(movieData);
+    // const html = await renderHomePage(movieData.results);
+    // res.send(html);
+  } catch (error) {
+    res.status(500).send("서버 오류가 발생했습니다.");
+  }
+});
+
+app.get("/detail/:id", async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const movieDetail = await moviesApi.getDetail(id);
+    res.json(movieDetail);
+    // const html = await renderDetailPage(movieDetail);
+    // res.send(html);
+  } catch (error) {
+    res.status(500).send("서버 오류가 발생했습니다.");
+  }
 });
 
 // public 폴더 속 정적 파일을 웹에서 접근할 수 있도록 만든다.
