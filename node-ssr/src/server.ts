82bde8163ca5dd25a86ebcfc4e824d31f969dcba
minji2219 <aabb0219@naver.com>
2025-10-18 17:09:42 +0900

    feat: serever 연결

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -1,30 +1,31 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Request, Response } from "express";
-import path from "path";
+import express, { Request, Response } from 'express';
+import path from 'path';
+import { moviesApi } from './service/tmdbApi';
+import { html } from './html';
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/", async (_req: Request, res: Response) => {
-  res.send(/*html*/ `
-    <!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <title>영화 리뷰</title>
-      </head>
-      <body>
-        테스트
-      </body>
-    </html>
-        `);
+app.get('/', async (_req: Request, res: Response) => {
+  const movies = (await moviesApi.getPopular()).results;
+  res.send(html.mainHtml(movies));
 });
 
-// public 폴더 속 정적 파일을 웹에서 접근할 수 있도록 만든다.
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/detail/:id', async (_req: Request, res: Response) => {
+  const movies = (await moviesApi.getPopular()).results;
+
+  const id = _req.params.id;
+  const movie = await moviesApi.getDetail(Number(id));
+
+  res.send(html.mainHtml(movies, html.modalHtml(movie)));
+});
 
 app.listen(PORT, (): void => {
   console.log(`🌟 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
