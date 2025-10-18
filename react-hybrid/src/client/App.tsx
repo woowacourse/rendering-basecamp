87b011dee88794a0263bd97ff609836a11849243
kimyou1102 <yoosk5485@naver.com>
2025-10-19 01:08:28 +0900

    feat: 페이지별 라우팅 처리

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MovieHomePage from "./pages/MovieHomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
 
 function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+  return (
+    <OverlayProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MovieHomePage />} />
+          <Route path="/detail/:movieId" element={<MovieDetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </OverlayProvider>
+  );
 }
-
 export default App;
