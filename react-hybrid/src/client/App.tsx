e7c3a590d45cf8030116b02525644431ca13e838
kimyou1102 <yoosk5485@naver.com>
2025-10-19 01:48:01 +0900

    refactor: MovieHomePage에 SSR 초기 movies 데이터 전달하도록 수정

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -3,16 +3,22 @@ import { OverlayProvider } from "overlay-kit";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MovieHomePage from "./pages/MovieHomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
+import { MovieItem } from "./types/Movie.types";
 
-function App() {
+type InitialData = {
+  movies?: MovieItem[];
+};
+
+function App({ initialData }: { initialData: InitialData }) {
   return (
     <OverlayProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MovieHomePage />} />
-          <Route path="/detail/:movieId" element={<MovieDetailPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<MovieHomePage movies={initialData.movies} />}
+        />
+        <Route path="/detail/:movieId" element={<MovieDetailPage />} />
+      </Routes>
     </OverlayProvider>
   );
 }
