fcf21b4b8d9941dd87b58d09df5e02cbca3cafde
mlnwns <mlnwns@naver.com>
2025-10-18 19:50:21 +0900

    feat: 영화 목록을 가져오는 기능 및 HTML 템플릿 개선

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -1,26 +1,91 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { Request, Response } from "express";
+import express from "express";
 import path from "path";
+import { moviesApi } from "./service/tmdbApi";
+import { Movie } from "./service/types";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/", async (_req: Request, res: Response) => {
+async function getMovies() {
+  const res = await moviesApi.getPopular();
+  return res.results;
+}
+
+function renderMovies(movies: Movie[]) {
+  return movies
+    .map(
+      (movie) => `
+      <li class="movie-item">
+          <div class="item">
+            <img
+              class="thumbnail"
+              src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+              alt="${movie.title}"
+              loading="lazy"
+            />
+            <div class="item-desc">
+              <p class="rate">
+                <img src="/images/star_empty.png" class="star" />
+                <span>${movie.vote_average.toFixed(1)}</span>
+              </p>
+              <strong>${movie.title}</strong>
+            </div>
+          </div>
+        </li>`
+    )
+    .join("");
+}
+
+app.get("/", async (_, res) => {
+  const movies = await getMovies();
+
   res.send(/*html*/ `
-    <!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <title>영화 리뷰</title>
-      </head>
-      <body>
-        테스트
-      </body>
-    </html>
-        `);
+  <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/styles/index.css" />
+    <title>영화 리뷰</title>
+  </head>
+  <body>
+    <div id="wrap">
+      <header>
+        <div class="background-container" style="background-image: url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg);">
+          <div class="overlay"></div>
+          <div class="top-rated-container">
+            <img src="/images/logo.png" width="117" height="20" class="logo" alt="MovieLogo" />
+            <div class="top-rated-movie">
+              <div class="rate">
+                <img src="/images/star_empty.png" width="32" height="32" />
+                <span class="text-2xl font-semibold text-yellow">7.7</span>
+              </div>
+              <h1 class="text-3xl font-semibold">인사이드 아웃 2</h1>
+              <button class="primary detail">자세히 보기</button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section class="container">
+          <h2 class="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
+          <ul class="thumbnail-list">
+            ${renderMovies(movies)}
+          </ul>
+        </section>
+      </main>
+      <footer class="footer">
+        <p>&copy; 우아한테크코스 All Rights Reserved.</p>
+        <p><img src="/images/woowacourse_logo.png" width="180" alt="우아한테크코스" /></p>
+      </footer>
+    </div>
+  </body>
+  </html>`);
 });
 
 // public 폴더 속 정적 파일을 웹에서 접근할 수 있도록 만든다.
