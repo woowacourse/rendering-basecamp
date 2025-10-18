6fd3d58aa3b32a503ec1c2a00b686e46c6ed9144
wo-o29 <woogur29@gmail.com>
2025-10-19 01:10:44 +0900

    feat: hydration 측정 코드 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -5,7 +5,17 @@ import { routes } from "./routes";
 const { path, props } = window.__INITIAL_DATA__;
 const Component = routes.find((route) => route.path === path).component;
 
+performance.mark("beforeHydrate");
 hydrateRoot(
   document.getElementById("root"),
+
   <App Component={Component} props={props} />
 );
+
+requestIdleCallback(() => {
+  performance.mark("afterHydrate");
+  performance.measure("hydration", "beforeHydrate", "afterHydrate");
+
+  const measure = performance.getEntriesByName("hydration")[0];
+  console.log(`Hydration이 걸린 시간: ${measure.duration}ms`);
+});
