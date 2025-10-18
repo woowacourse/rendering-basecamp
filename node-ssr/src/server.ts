378fe1e18ba36a44c474b6a347c6726d285269ae
Beomtae <region9@naver.com>
2025-10-18 14:08:25 +0900

    메인 페이지 랜더링

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -3,21 +3,96 @@ dotenv.config();
 
 import express, { Request, Response } from "express";
 import path from "path";
+import { moviesApi } from "./service/tmdbApi";
+import { Movie } from "./service/types";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/", async (_req: Request, res: Response) => {
+const renderMovies = (movies: Movie[]) => {
+  return movies
+    .map(
+      (movie) => `
+      <li class="movie-item">
+        <div class="item">
+          <img
+            class="thumbnail"
+            src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+            alt="${movie.title}"
+            loading="lazy"
+          />
+          <div class="item-desc">
+            <p class="rate">
+              <img src="/images/star_empty.png" class="star" />
+              <span>${movie.vote_average.toFixed(1)}</span>
+            </p>
+            <strong>${movie.title}</strong>
+          </div>
+        </div>
+      </li>`
+    )
+    .join("");
+};
+
+app.get("/", async (req: Request, res: Response) => {
+  const movies = (await moviesApi.getPopular(1)).results;
+  const imageUrl = movies[0].poster_path;
+  const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+
   res.send(/*html*/ `
-    <!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <title>영화 리뷰</title>
-      </head>
-      <body>
-        테스트
+   <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Movie App" />
+    <meta property="og:description" content="영화를 소개시켜주는 서비스입니다." />
+    <meta property="og:image" content=${imageUrl} />
+    <meta property="og:url" content=${url} />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Movie App" />
+    <link rel="stylesheet" href="/styles/index.css" />
+    <title>영화 리뷰</title>
+  </head>
+  <body>
+    <div id="wrap">
+      <header>
+        <div class="background-container" style="background-image: url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg);">
+          <div class="overlay"></div>
+          <div class="top-rated-container">
+            <img src="/images/logo.png" width="117" height="20" class="logo" alt="MovieLogo" />
+            <div class="top-rated-movie">
+              <div class="rate">
+                <img src="/images/star_empty.png" width="32" height="32" />
+                <span class="text-2xl font-semibold text-yellow">7.7</span>
+              </div>
+              <h1 class="text-3xl font-semibold">인사이드 아웃 2</h1>
+              <button class="primary detail">자세히 보기</button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section class="container">
+          <h2 class="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
+          <ul class="thumbnail-list">
+            ${renderMovies(movies)}
+          </ul>
+        </section>
+      </main>
+      <footer class="footer">
+        <p>&copy; 우아한테크코스 All Rights Reserved.</p>
+        <p><img src="/images/woowacourse_logo.png" width="180" alt="우아한테크코스" /></p>
+      </footer>
+    </div>
+  </body>
+  </html>
+        `);
+});
+
+  res.send(/*html*/ `
       </body>
     </html>
         `);
