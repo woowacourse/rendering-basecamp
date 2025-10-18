a9d66ade8db05b2ddd46fcfd85f9aae1461b1dcb
wo-o29 <woogur29@gmail.com>
2025-10-18 23:42:15 +0900

    refactor: 메인 페이지 수정

--- react-hybrid/src/client/pages/MovieHomePage.tsx
+++ react-hybrid/src/client/pages/MovieHomePage.tsx
@@ -1,24 +1,29 @@
-import { Header } from '../components/Header';
-import { MovieList } from '../components/MovieList';
-import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
+import { Header } from "../components/Header";
+import { MovieList } from "../components/MovieList";
+import { Footer } from "../components/Footer";
+import { moviesApi } from "../api/movies";
+import type { InferGetServerDataType } from "../types";
 
-export default function MovieHomePage() {
-  const { data: movies, isLoading } = usePopularMovies();
-
-  if (isLoading === true) {
-    return <Loading />;
+export const getServerData = async () => {
+  try {
+    const popularMovie = await moviesApi.getPopular();
+    return { popularMovie: popularMovie.data.results };
+  } catch (error) {
+    return { popularMovie: null };
   }
+};
 
-  if (movies == null || movies.length === 0) {
+type MovieHomePageProps = InferGetServerDataType<typeof getServerData>;
+
+export default function MovieHomePage({ popularMovie }: MovieHomePageProps) {
+  if (popularMovie == null || popularMovie.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
   return (
     <div id="wrap">
-      <Header featuredMovie={movies[0]} />
-      <MovieList movies={movies} />
+      <Header featuredMovie={popularMovie[0]} />
+      <MovieList movies={popularMovie} />
       <Footer />
     </div>
   );
