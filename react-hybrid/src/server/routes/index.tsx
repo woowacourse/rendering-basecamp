c5bdc9a631a61508e2cdfd628a065ec5518abc74
jeongyou <wjddbwjd1205@naver.com>
2025-10-19 02:41:45 +0900

    feat: 서버에서 인기 영화 SSR 렌더링

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
-import { renderToString } from "react-dom/server";
-import App from "../../client/App";
-import React from "react";
+import { renderToString } from 'react-dom/server';
+import App from '../../client/App';
+import React from 'react';
+import { moviesApi } from '../../client/api/movies';
 
 const router = Router();
 
@@ -26,27 +27,34 @@ function generateHTML() {
     `;
 }
 
-router.get("/", (_: Request, res: Response) => {
+router.get('/', async (_: Request, res: Response) => {
   const template = generateHTML();
+  try {
+    const popularMovies = await moviesApi.getPopular();
+    const movies = popularMovies.data?.results ?? [];
 
-  const renderedApp = renderToString(<App />);
+    const renderedApp = renderToString(<App initialMovies={movies} />);
 
-  const renderedHTMLWithInitialData = template.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    /*html*/ `
-    <script>
+    const renderedHTMLWithInitialData = template.replace(
+      '<!--{INIT_DATA_AREA}-->',
+      /*html*/ `
+      <script>
       window.__INITIAL_DATA__ = {
-        movies: ${JSON.stringify([])}
-      }
-    </script>
-  `
-  );
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+        movies: ${JSON.stringify(movies)}
+        }
+        </script>
+        `
+    );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      '<!--{BODY_AREA}-->',
+      renderedApp
+    );
 
-  res.send(renderedHTML);
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('영화 데이터를 불러오는 중 오류가 발생했습니다.');
+  }
 });
 
 export default router;
