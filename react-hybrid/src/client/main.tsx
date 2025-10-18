02bce9d35b51d7daa066476343ca7f81239d9248
kimyou1102 <yoosk5485@naver.com>
2025-10-19 01:48:13 +0900

    feat: SSR 초기 데이터 하이드레이션 위해 App에 props 전달

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const initialData = window.__INITIAL_DATA__;
 console.log("initialData", initialData);
 
-hydrateRoot(document.getElementById("root"), <App />);
+hydrateRoot(
+  document.getElementById("root"),
+  <BrowserRouter>
+    <App initialData={initialData} />
+  </BrowserRouter>
+);
