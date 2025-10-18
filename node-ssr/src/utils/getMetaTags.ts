3b3fab7486e42c38d04181a02fa78a78bc265736
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-10-18 15:32:44 +0900

    feat: 메타태그 생성 함수 추가

---
+++ node-ssr/src/utils/getMetaTags.ts
@@ -0,0 +1,28 @@
+interface GetMetaTagsParams {
+  title: string;
+  description: string;
+  imageUrl: string;
+  pageUrl: string;
+}
+
+const getMetaTags = ({
+  title,
+  description,
+  imageUrl,
+  pageUrl,
+}: GetMetaTagsParams) => `
+  <title>${title}</title>
+  <meta name="description" content='${description}' />
+
+  <meta property="og:title" content='${title}' />
+  <meta property="og:description" content='${description}' />
+  <meta property="og:image" content='${imageUrl}' />
+  <meta property="og:url" content='${pageUrl}' />
+  <meta property="og:type" content="website" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content='${title}' />
+  <meta name="twitter:description" content='${description}' />
+  <meta name="twitter:image" content='${imageUrl}' />
+  <meta name="twitter:url" content='${pageUrl}' />
+`;
