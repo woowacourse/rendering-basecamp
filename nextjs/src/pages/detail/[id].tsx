70654b55065e932382fbbc29f8113ac214deff9d
spoyodevelop <kjy9505@gmail.com>
2025-10-18 16:21:31 +0900

    refactor: 서버 사이드 렌더링을 위한 getServerSideProps 통합 및 MovieDetailPage 컴포넌트 개선

--- nextjs/src/pages/detail/[id].tsx
+++ nextjs/src/pages/detail/[id].tsx
@@ -2,27 +2,50 @@ import { useMovieDetailModal } from "../../hooks/useMovieDetailModal";
 import { useEffect, useRef } from "react";
 import { moviesApi } from "../../api/movies";
 import MovieHomePage, { fetchMoviesForSSR } from "../MovieHomePage";
-import type { MovieItem } from "../../types/Movie.types";
-import type { MovieDetailResponse } from "../../types/MovieDetail.types";
-import type { GetServerSidePropsContext } from "next/types";
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import Head from "next/head";
 
-interface MovieDetailPageProps {
-  movies: MovieItem[] | null;
-  movieDetail: MovieDetailResponse | null;
-}
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
-export default function MovieDetailPage({
-  movies,
-  movieDetail,
-}: MovieDetailPageProps) {
-  if (movies == null || movieDetail == null) {
-    return <div>영화 정보를 불러오는데 실패했습니다.</div>;
+export const getServerSideProps = (async (context) => {
+  const id = Number(context.params?.id);
+
+  try {
+    const [moviesRes, movieDetailRes] = await Promise.all([
+      fetchMoviesForSSR(),
+      moviesApi.getDetail(id),
+    ]);
+
+    const movies = moviesRes.props?.movies;
+    const movieDetail = movieDetailRes.data;
+
+    if (!movies || !movieDetail) return { notFound: true };
+
+    return { props: { movies, movieDetail } };
+  } catch {
+    return { notFound: true };
   }
+}) satisfies GetServerSideProps;
+
+const MovieDetailPage: NextPage<Props> = ({ movies, movieDetail }) => {
+  const { openMovieDetailModal } = useMovieDetailModal();
+  const onceRef = useRef(false);
+
+  useEffect(() => {
+    if (onceRef.current) return;
+
+    onceRef.current = true;
+    openMovieDetailModal(movieDetail);
+  }, [movieDetail, openMovieDetailModal]);
 
   const imageUrl = movieDetail.poster_path
-    ? "https://image.tmdb.org/t/p/w500/" + movieDetail.poster_path
+    ? `https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`
     : "/images/no_image.png";
+
   return (
     <>
       <Head>
@@ -34,66 +57,8 @@ export default function MovieDetailPage({
         movies={movies}
         pageTitle={`${movieDetail.title} - MovieList`}
       />
-      <DetailPageOpenModal initialMovieDetail={movieDetail} />
     </>
   );
-}
-
-function DetailPageOpenModal({
-  initialMovieDetail,
-}: {
-  initialMovieDetail: MovieDetailResponse;
-}) {
-  const { openMovieDetailModal } = useMovieDetailModal();
-  const onceRef = useRef(false);
-
-  useEffect(() => {
-    if (initialMovieDetail == null || onceRef.current === true) {
-      return;
-    }
-    onceRef.current = true;
-    openMovieDetailModal(initialMovieDetail);
-  }, [initialMovieDetail, openMovieDetailModal]);
-
-  return null;
-}
-
-const fetchDetailForSSR = async (context: GetServerSidePropsContext) => {
-  try {
-    const { id } = context.params as { id: string | null };
-
-    if (id == null) {
-      return { props: {} };
-    }
-
-    const movieDetail = await moviesApi.getDetail(Number(id));
-
-    return {
-      props: {
-        movieDetail: movieDetail.data,
-      },
-    };
-  } catch {
-    return { props: {} };
-  }
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-): Promise<{ props: MovieDetailPageProps }> => {
-  try {
-    const [moviesRes, detailRes] = await Promise.all([
-      fetchMoviesForSSR(),
-      fetchDetailForSSR(context),
-    ]);
-
-    return {
-      props: {
-        movies: moviesRes.props.movies ?? null,
-        movieDetail: detailRes.props.movieDetail ?? null,
-      },
-    };
-  } catch {
-    return { props: { movies: null, movieDetail: null } };
-  }
-};
+export default MovieDetailPage;
