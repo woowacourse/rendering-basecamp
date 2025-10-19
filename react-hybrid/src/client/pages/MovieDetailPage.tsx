e243cf6d285262b021fab6a6dc402377a59b6733
spoyodevelop <kjy9505@gmail.com>
2025-10-19 17:42:20 +0900

    feat: moviedeatil 페이지에 SSR 적용

--- react-hybrid/src/client/pages/MovieDetailPage.tsx
+++ react-hybrid/src/client/pages/MovieDetailPage.tsx
@@ -1,33 +1,60 @@
-import { useMovieDetailModal } from '../hooks/useMovieDetailModal';
-import { useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import MovieHomePage from './MovieHomePage';
-import { moviesApi } from '../api/movies';
+import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import { useEffect, useRef } from "react";
+import MovieHomePage from "./MovieHomePage";
+import { MovieResponse } from "../types/Movie.types";
+import { MovieDetailResponse } from "../types/MovieDetail.types";
+import { ApiResult } from "../types/ApiResult.types";
 
-export default function MovieDetailPage() {
+interface MovieDetailPageProps {
+  popularMoviesResult: ApiResult<MovieResponse>;
+  movieDetailResult: ApiResult<MovieDetailResponse>;
+}
+export default function MovieDetailPage({
+  popularMoviesResult,
+  movieDetailResult,
+}: MovieDetailPageProps) {
+  const { data: movies, error: popularMoviesError } = popularMoviesResult;
+  const { data: movieDetail, error: movieDetailError } = movieDetailResult;
+
+  if (popularMoviesError) {
+    return <div> 꺄악! 고장났슈! {popularMoviesError.message}</div>;
+  }
+
+  if (movieDetailError) {
+    return <div> 꺄악! 고장났슈! {movieDetailError.message}</div>;
+  }
+
+  if (!movies || !movies.results || movies.results.length === 0) {
+    return <div>영화 데이터가 없습니다.</div>;
+  }
+
+  if (!movieDetail) {
+    return <div>영화 상세 정보가 없습니다.</div>;
+  }
+
   return (
     <>
-      <MovieHomePage />
-      <DetailPageOpenModal />
+      <MovieHomePage popularMoviesResult={popularMoviesResult} />
+      <DetailPageOpenModal movieDetail={movieDetail} />
     </>
   );
 }
 
-function DetailPageOpenModal() {
-  const { movieId } = useParams();
+function DetailPageOpenModal({
+  movieDetail,
+}: {
+  movieDetail: MovieDetailResponse;
+}) {
   const { openMovieDetailModal } = useMovieDetailModal();
   const onceRef = useRef(false);
 
   useEffect(() => {
-    if (movieId == null || onceRef.current === true) {
+    if (!movieDetail || onceRef.current) {
       return;
     }
-    (async () => {
-      onceRef.current = true;
-      const movieDetail = await moviesApi.getDetail(Number(movieId));
-      openMovieDetailModal(movieDetail.data);
-    })();
-  }, [movieId, openMovieDetailModal]);
+    onceRef.current = true;
+    openMovieDetailModal(movieDetail);
+  }, [movieDetail, openMovieDetailModal]);
 
   return null;
 }
