54097d4c2cf26e0808684c61aeac8ad013c76c0e
spoyodevelop <kjy9505@gmail.com>
2025-10-19 18:05:54 +0900

    feat: HTML 및 메타 태그 생성 함수 추가

---
+++ react-hybrid/src/server/util/generateHTML.ts
@@ -0,0 +1,51 @@
+import { MovieDetailResponse } from "../../client/types/MovieDetail.types";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+const generateHTML = () => {
+  return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="ko">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="/static/styles/index.css" />
+          <title>영화 리뷰</title>
+          <!--{OG_TAGS}-->
+        </head>
+        <body>
+          <div id="root"><!--{BODY_AREA}--></div>
+          <!--{INIT_DATA_AREA}-->
+          <script src="/static/bundle.js"></script>
+        </body>
+      </html>
+      `;
+};
+const generateMeta = (movieDetailResponse?: MovieDetailResponse) => {
+  if (!movieDetailResponse) {
+    return `
+        <title>마빈의 영화 리뷰</title>
+        <meta property="og:title" content="마빈의 영화 리뷰" />
+        <meta property="og:description" content="마빈의 영화 리뷰1 !!!!" />
+        <meta property="og:image" content="/images/logo.png" />
+      `;
+  }
+  return `
+      <title>${movieDetailResponse.title}</title>
+      <meta property="og:title" content="${
+        "마빈의 영화 리뷰 | " + movieDetailResponse.title
+      }" />
+      <meta
+        property="og:description"
+        content="${movieDetailResponse.overview}"
+      />
+      <meta
+        property="og:image"
+        content="${
+          movieDetailResponse.poster_path
+            ? IMAGE_BASE_URL + movieDetailResponse.poster_path
+            : "/images/no-poster.png"
+        }"
+      />
+    `;
+};
+export { generateHTML, generateMeta };
