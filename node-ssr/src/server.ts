3e1fc93db7e5c0e427acf731f1aefee71eaaf9e5
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-19 10:47:10 +0900

    feat: 영화 상세 정보 html 만들기 기능 추가

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -91,6 +91,59 @@ app.get('/', async (_req: Request, res: Response) => {
 const getDetailMovie = async (movieId: number) => {
   return await moviesApi.getDetail(movieId);
 };
+
+const generateDetailMovieHTML = (movie: MovieDetail) => {
+  return /*html*/ `
+  
+  <div class="modal-background active">
+      <div class="modal">
+        <!-- 모달 헤더 -->
+        <div class="modal-header">
+          <h1 class="modal-title">${movie.title}</h1>
+          <img src="/images/modal_button_close.png" width="24" height="24" class="modal-close-btn" alt="Close" />
+        </div>
+
+        <div class="modal-container">
+          <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${
+    movie.title
+  }" class="modal-image" />
+          <div class="modal-description">
+            <!-- 영화 정보 섹션 -->
+            <div class="movie-info-line">
+              <span class="movie-meta">${movie.genres.map(({ name }) => name).join(', ')}</span>
+              <div class="movie-rating">
+                <img src="/images/star_filled.png" width="16" height="16" />
+                <span class="rating-value">${movie.vote_average}</span>
+              </div>
+            </div>
+
+            <!-- 줄거리 -->
+            <div class="overview-section">
+              <p class="overview-text">
+                ${movie.overview}
+              </p>
+            </div>
+
+            <!-- 내 별점 섹션 -->
+            <div class="my-rating-section">
+              <div class="rating-header">
+                <span class="rating-label">내 별점</span>
+                <div class="star-rating">
+                  <img src="/images/star_filled.png" width="24" height="24" alt="Star 1" />
+                  <img src="/images/star_filled.png" width="24" height="24" alt="Star 2" />
+                  <img src="/images/star_filled.png" width="24" height="24" alt="Star 3" />
+                  <img src="/images/star_filled.png" width="24" height="24" alt="Star 4" />
+                  <img src="/images/star_empty.png" width="24" height="24" alt="Star 5" />
+                  <span class="rating-text">8 재미있어요</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
         `);
 });
 
