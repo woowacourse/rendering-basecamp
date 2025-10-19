ffb92730fd30487391fe42dac6293ef476023d6c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-19 10:46:22 +0900

    feat: 홈페이지 html 서버에서 보내주는 기능 추가

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -36,16 +36,57 @@ const generateMovieListHTML = (movies: Movie[]) => {
     .join('');
 };
 
+app.get('/', async (_req: Request, res: Response) => {
+  const popularMovies = await getPopularMovies();
+
+  const featuredMovie = popularMovies[0];
   res.send(/*html*/ `
-    <!DOCTYPE html>
+   <!DOCTYPE html>
     <html lang="ko">
       <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles/index.css" />
         <title>영화 리뷰</title>
       </head>
       <body>
-        테스트
+        <div id="wrap">
+          <header>
+            <div class="background-container" style="background-image: url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg);">
+              <div class="overlay"></div>
+              <div class="top-rated-container">
+                <img src="/images/logo.png" width="117" height="20" class="logo" alt="MovieLogo" />
+                <div class="top-rated-movie">
+                  <div class="rate">
+                    <img src="/images/star_empty.png" width="32" height="32" />
+                    <span class="text-2xl font-semibold text-yellow">${featuredMovie.vote_average}</span>
+                  </div>
+                  <h1 class="text-3xl font-semibold">${featuredMovie.title}</h1>
+                  <button class="primary detail">자세히 보기</button>
+                </div>
+              </div>
+            </div>
+          </header>
+          <main>
+            <section class="container">
+              <h2 class="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
+              <ul class="thumbnail-list">
+                ${generateMovieListHTML(popularMovies)}
+              </ul>
+            </section>
+          </main>
+          <footer class="footer">
+            <p>&copy; 우아한테크코스 All Rights Reserved.</p>
+            <p><img src="/images/woowacourse_logo.png" width="180" alt="우아한테크코스" /></p>
+          </footer>
+        </div>
       </body>
     </html>
+
+    
+
+        `);
+});
         `);
 });
 
