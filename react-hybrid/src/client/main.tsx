6a80d2483dff982a99e635f503e70262e0110a6b
spoyodevelop <kjy9505@gmail.com>
2025-10-19 17:33:25 +0900

    feat: 컴포넌트별로 이름을 매핑

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
+import MovieHomePage from "./pages/MovieHomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
 
-const initialData = window.__INITIAL_DATA__;
+const initialData = window.__INITIAL_DATA__ as {
+  Component: string;
+  props: any;
+};
 console.log("initialData", initialData);
 
-hydrateRoot(document.getElementById("root"), <App />);
+// Component 이름으로 실제 컴포넌트 매핑
+const COMPONENTS: Record<string, React.ComponentType<any>> = {
+  MovieHomePage,
+  MovieDetailPage,
+};
+
+const Component = COMPONENTS[initialData.Component];
+
+hydrateRoot(
+  document.getElementById("root")!,
+  <App Component={Component} props={initialData.props} />
+);
