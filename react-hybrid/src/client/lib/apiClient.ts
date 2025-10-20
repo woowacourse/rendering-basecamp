88cadee22c2c3465913c6ace78b213c8edad3840
guesung <gueit214@naver.com>
2025-10-20 17:35:53 +0900

    refactor: console.log 제거

--- react-hybrid/src/client/lib/apiClient.ts
+++ react-hybrid/src/client/lib/apiClient.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-console.log(process.env);
-console.log(process.env.TMDB_ACCESS_TOKEN);
-
 export const apiClient = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
