0dc0ca95d660bc9b1b772173e190c6ec9b0cdf3b
guesung <gueit214@naver.com>
2025-10-20 16:00:05 +0900

    chore: 디버깅용 console.log

--- react-hybrid/src/client/lib/apiClient.ts
+++ react-hybrid/src/client/lib/apiClient.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+console.log(process.env);
+console.log(process.env.TMDB_ACCESS_TOKEN);
+
 export const apiClient = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
