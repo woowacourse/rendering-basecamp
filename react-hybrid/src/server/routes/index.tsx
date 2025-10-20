6e83011ec48345462392b3115b38b4fdb81f692f
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-20 17:52:26 +0900

    feat: 영화 목록 및 상세 페이지에 대한 SSR 구현 및 정적 파일 서빙 경로 추가

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import { renderToString } from "react-dom/server";
 import App from "../../client/App";
 import React from "react";
+import axios from "axios";
 
 const router = Router();
 
@@ -26,27 +27,93 @@ function generateHTML() {
     `;
 }
 
-router.get("/", (_: Request, res: Response) => {
-  const template = generateHTML();
+router.get("/", async (_: Request, res: Response) => {
+  try {
+    const template = generateHTML();
 
-  const renderedApp = renderToString(<App />);
-
-  const renderedHTMLWithInitialData = template.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    /*html*/ `
-    <script>
-      window.__INITIAL_DATA__ = {
-        movies: ${JSON.stringify([])}
+    const popularResponse = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?page=1&language=ko-KR`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
       }
-    </script>
-  `
-  );
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+    );
 
-  res.send(renderedHTML);
+    const movies = popularResponse.data?.results ?? [];
+
+    const renderedApp = renderToString(<App initialMovies={movies} />);
+
+    const renderedHTMLWithInitialData = template.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      /*html*/ `
+      <script>
+        window.__INITIAL_DATA__ = ${JSON.stringify({ movies })}
+      </script>
+    `
+    );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/detail/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const template = generateHTML();
+
+    const [popularResponse, detailResponse] = await Promise.all([
+      axios.get(
+        `https://api.themoviedb.org/3/movie/popular?page=1&language=ko-KR`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      ),
+      axios.get(`https://api.themoviedb.org/3/movie/${id}?language=ko-KR`, {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }),
+    ]);
+
+    const movies = popularResponse.data?.results ?? [];
+    const detail = detailResponse.data ?? null;
+
+    const renderedApp = renderToString(
+      <App initialMovies={movies} initialDetail={detail} />
+    );
+
+    const initialData = { movies, detail };
+    const renderedHTMLWithInitialData = template.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      /*html*/ `
+      <script>
+        window.__INITIAL_DATA__ = ${JSON.stringify(initialData)}
+      </script>
+    `
+    );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
