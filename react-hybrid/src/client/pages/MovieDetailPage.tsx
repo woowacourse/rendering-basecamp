2e79956b3e756fcf0def4669f0b6c023f047c5c2
Jeongeun Lee <leeju1009@gmail.com>
2025-10-21 03:17:19 +0900

    refactor: 클라이언트에서 window 객체에 접근하도록 코드 위치 이동

--- react-hybrid/src/client/pages/MovieDetailPage.tsx
+++ react-hybrid/src/client/pages/MovieDetailPage.tsx
@@ -1,7 +1,7 @@
-import { useMovieDetailModal } from '../hooks/useMovieDetailModal';
-import { useEffect, useRef } from 'react';
-import MovieHomePage from './MovieHomePage';
-import { moviesApi } from '../api/movies';
+import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import { useEffect, useRef } from "react";
+import MovieHomePage from "./MovieHomePage";
+import { moviesApi } from "../api/movies";
 
 export default function MovieDetailPage() {
   return (
@@ -13,11 +13,12 @@ export default function MovieDetailPage() {
 }
 
 function DetailPageOpenModal() {
-  const movieId = window.__INITIAL_DATA__?.movieId;
   const { openMovieDetailModal } = useMovieDetailModal();
   const onceRef = useRef(false);
 
   useEffect(() => {
+    const movieId = window.__INITIAL_DATA__?.movieId;
+
     if (movieId == null || onceRef.current === true) {
       return;
     }
@@ -26,7 +27,7 @@ function DetailPageOpenModal() {
       const movieDetail = await moviesApi.getDetail(Number(movieId));
       openMovieDetailModal(movieDetail.data);
     })();
-  }, [movieId, openMovieDetailModal]);
+  }, [openMovieDetailModal]);
 
   return null;
 }
