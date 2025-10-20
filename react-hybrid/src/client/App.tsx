a03e49e936adad4e449f8507d84862091c824dd4
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-20 17:55:16 +0900

    feat: App 컴포넌트에 초기 영화 데이터 및 상세 정보를 props로 전달하여 SSR 지원

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import type { MovieItem } from "./types/Movie.types";
+import type { MovieDetailResponse } from "./types/MovieDetail.types";
+import MovieHomePage from "./pages/MovieHomePage";
+import { MovieDetailModal } from "./components/MovieDetailModal";
 
-function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+interface AppProps {
+  initialMovies?: MovieItem[];
+  initialDetail?: MovieDetailResponse | null;
+}
+
+function App({ initialMovies, initialDetail }: AppProps) {
+  const handleCloseModal = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <OverlayProvider>
+      <MovieHomePage initialMovies={initialMovies} />
+      {initialDetail ? (
+        <MovieDetailModal movie={initialDetail} onClose={handleCloseModal} />
+      ) : null}
+    </OverlayProvider>
+  );
 }
 
 export default App;
--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
-const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+const initialData = window.__INITIAL_DATA__ ?? {};
 
-hydrateRoot(document.getElementById("root"), <App />);
+hydrateRoot(
+  document.getElementById("root")!,
+  <App initialMovies={initialData.movies} initialDetail={initialData.detail} />
+);
