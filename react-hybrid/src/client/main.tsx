eb64020f385413e90895194bccba9b8ec44dc7a2
sooyeoniya <tndus1306403@naver.com>
2025-10-22 01:41:41 +0900

    perf: hydration 성능 측정 로직 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -4,7 +4,10 @@ import App from "./App";
 
 const { routeType, initialData } = window.__INITIAL_DATA__;
 
+performance.mark("beforeRender");
 hydrateRoot(
   document.getElementById("root"),
   <App routeType={routeType} initialData={initialData} />
 );
+performance.mark("afterHydrate");
+performance.measure("hydration", "beforeRender", "afterHydrate");
