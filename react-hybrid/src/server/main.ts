8412b6262fb1fe148d5644254a0fb6a0436ff3db
SuHwa Lee <tnghk9611@naver.com>
2025-10-22 00:42:01 +0900

    chore: 정적 파일 제공하도록 하기

--- react-hybrid/src/server/main.ts
+++ react-hybrid/src/server/main.ts
@@ -13,6 +13,15 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "../../dist/static/images"))
+);
+app.use(
+  "/styles",
+  express.static(path.join(__dirname, "../../dist/static/styles"))
+);
+
 app.use("/static", express.static(path.join(__dirname, "../../dist/static")));
 
 app.use("/", movieRouter);
