75754c2f0be51f543361798d9f5a055f8343023a
eunwoo-levi <eunwoo1341@gmail.com>
2025-10-21 17:27:18 +0900

    feat: server에서 페이지를 ssr로 응답주는 server로직 구현

--- node-ssr/src/server.ts
+++ node-ssr/src/server.ts
@@ -1,33 +1,22 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Request, Response } from "express";
-import path from "path";
+import express from 'express';
+import { indexPage } from './routes/indexPage';
+import path from 'path';
+import { detailPage } from './routes/detailPage';
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/", async (_req: Request, res: Response) => {
-  res.send(/*html*/ `
-    <!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <title>영화 리뷰</title>
-      </head>
-      <body>
-        테스트
-      </body>
-    </html>
-        `);
-});
+app.get('/', indexPage);
+app.get('/detail/:id', detailPage);
 
-// public 폴더 속 정적 파일을 웹에서 접근할 수 있도록 만든다.
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, (): void => {
   console.log(`🌟 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
-
 export default app;
