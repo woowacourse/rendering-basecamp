5091c7f3153fc97f09cc4ceb45f37bd9ea8f4c2e
SuHwa Lee <tnghk9611@naver.com>
2025-10-22 01:07:45 +0900

    fix: 하이드레이션 에러 해결

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -4,9 +4,11 @@ import { MovieItem } from "./types/Movie.types";
 import MovieHomePage from "./pages/MovieHomePage";
 
 function App({ movies }: { movies: MovieItem[] }) {
-  return <OverlayProvider>
-    <MovieHomePage movies={movies} />
-  </OverlayProvider>;
+  return (
+    <OverlayProvider>
+      <MovieHomePage movies={movies} />
+    </OverlayProvider>
+  );
 }
 
 export default App;
