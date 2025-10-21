a95009dd7d95acca314ecceeba62d3faa87ac172
minji2219 <aabb0219@naver.com>
2025-10-21 21:44:10 +0900

    feat: hydrate 연결

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { OverlayProvider } from "overlay-kit";
+import React from 'react';
+import { OverlayProvider } from 'overlay-kit';
 
-function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+function App({ Component, initialData }) {
+  return (
+    <OverlayProvider>
+      <Component initialData={initialData} />
+    </OverlayProvider>
+  );
 }
 
 export default App;
