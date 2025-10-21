15250d02965f75649a2d6d2bc44a2408d9a80398
Ko Daeun <drechsu@gmail.com>
2025-10-21 22:46:04 +0900

    chore : server에서도 환경변수 읽게 수정

--- react-hybrid/webpack.server.config.js
+++ react-hybrid/webpack.server.config.js
@@ -1,37 +1,52 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// .env 파일 로드
+const env = dotenv.config().parsed;
+
+// 환경 변수를 webpack DefinePlugin에 전달할 형태로 변환
+const envKeys = Object.keys(env || {}).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
-  mode: "development",
-  target: "node",
-  entry: path.resolve(__dirname, "src/server/main.ts"),
+  mode: 'development',
+  target: 'node',
+  entry: path.resolve(__dirname, 'src/server/main.ts'),
   output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist/server"),
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist/server'),
   },
   externals: [nodeExternals()],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
+  plugins: [new webpack.DefinePlugin(envKeys)],
   module: {
     rules: [
       {
         test: /\.(jsx?|tsx?)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
-              "@babel/preset-env", 
-              ["@babel/preset-react", { "runtime": "automatic" }],
-              ["@babel/preset-typescript", { "isTSX": true, "allExtensions": true }]
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              [
+                '@babel/preset-typescript',
+                { isTSX: true, allExtensions: true },
+              ],
             ],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
