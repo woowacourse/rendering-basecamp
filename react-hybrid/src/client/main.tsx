aabca2ed33cef19d2a6734e17d7133388053693b
Ko Daeun <drechsu@gmail.com>
2025-10-22 01:06:49 +0900

    feat : performance  로깅 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import App from './App';
 
 const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+console.log('initialData', initialData);
 
-hydrateRoot(document.getElementById("root"), <App />);
+performance.mark('beforeRender');
+hydrateRoot(document.getElementById('root'), <App />);
+performance.mark('afterHydrate');
+performance.measure('hydration', 'beforeRender', 'afterHydrate');
