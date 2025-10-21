00755cb93b83df757fcb5946916f17e1bbcb04c4
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-21 14:23:04 +0900

    feat: 영화 상세 모달에서 URL 및 메타 데이터 관리 기능 추가

--- react-hybrid/src/client/hooks/useMovieDetailModal.tsx
+++ react-hybrid/src/client/hooks/useMovieDetailModal.tsx
@@ -1,14 +1,27 @@
-import { overlay } from 'overlay-kit';
-import { MovieDetailResponse } from '../types/MovieDetail.types';
-import { MovieDetailModal } from '../components/MovieDetailModal';
+import { overlay } from "overlay-kit";
+import { MovieDetailResponse } from "../types/MovieDetail.types";
+import { MovieDetailModal } from "../components/MovieDetailModal";
+import { applyMeta, buildDetailMeta, buildHomeMeta } from "../utils/meta";
 
 export const useMovieDetailModal = () => {
   const openMovieDetailModal = (movie: MovieDetailResponse) => {
-    return new Promise<void>(resolve => {
+    return new Promise<void>((resolve) => {
+      if (typeof window !== "undefined") {
+        window.history.pushState({}, "", `/detail/${movie.id}`);
+      }
+      applyMeta(buildDetailMeta(movie));
       overlay.open(({ unmount }) => (
         <MovieDetailModal
           movie={movie}
           onClose={() => {
+            if (typeof window !== "undefined") {
+              window.history.replaceState({}, "", "/");
+            }
+            const initialMovies =
+              typeof window !== "undefined"
+                ? window.__INITIAL_DATA__?.movies
+                : undefined;
+            applyMeta(buildHomeMeta(initialMovies));
             resolve();
             unmount();
           }}
