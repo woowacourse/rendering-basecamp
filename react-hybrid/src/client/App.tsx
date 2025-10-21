d7ef4c2b440ba6438717874ba9821ded4385826a
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-21 14:21:53 +0900

    refactor: App 컴포넌트에서 영화 상세 정보 관련 코드 제거

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
 import type { MovieItem } from "./types/Movie.types";
-import type { MovieDetailResponse } from "./types/MovieDetail.types";
 import MovieHomePage from "./pages/MovieHomePage";
-import { MovieDetailModal } from "./components/MovieDetailModal";
 
 interface AppProps {
   initialMovies?: MovieItem[];
-  initialDetail?: MovieDetailResponse | null;
 }
 
-function App({ initialMovies, initialDetail }: AppProps) {
-  const handleCloseModal = () => {
-    window.location.href = "/";
-  };
-
+function App({ initialMovies }: AppProps) {
   return (
     <OverlayProvider>
       <MovieHomePage initialMovies={initialMovies} />
-      {initialDetail ? (
-        <MovieDetailModal movie={initialDetail} onClose={handleCloseModal} />
-      ) : null}
     </OverlayProvider>
   );
 }
