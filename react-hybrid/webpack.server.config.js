e77241b158a94c84e75b2ace7977d11c37932308
Oliy <eunsoa0310@gmail.com>
2025-10-22 03:55:00 +0900

    chore: dotenv-webpack 사용하도록 webpack server configuration 수정

--- react-hybrid/webpack.server.config.js
+++ react-hybrid/webpack.server.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: "development",
-  target: "node",
-  entry: path.resolve(__dirname, "src/server/main.ts"),
+  mode: 'development',
+  target: 'node',
+  entry: path.resolve(__dirname, 'src/server/main.ts'),
   output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist/server"),
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist/server'),
   },
   externals: [nodeExternals()],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
@@ -19,20 +20,21 @@ module.exports = {
         test: /\.(jsx?|tsx?)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
-              "@babel/preset-env", 
-              ["@babel/preset-react", { "runtime": "automatic" }],
-              ["@babel/preset-typescript", { "isTSX": true, "allExtensions": true }]
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
             ],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  plugins: [new Dotenv()],
 };
