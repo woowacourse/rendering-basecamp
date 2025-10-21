d7a3fb7fcb87cdc084ee2bafcb76cc0ba690e6e4
keemsebeen <sebin0580@gmail.com>
2025-10-22 02:10:08 +0900

    feat: App 컴포넌트에 AppProps 추가

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { OverlayProvider } from "overlay-kit";
+import MovieHomePage from "./pages/MovieHomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import { MovieItem } from "./types/Movie.types";
 
-function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+interface AppProps {
+  initialMovies?: MovieItem[];
+}
+
+function App({ initialMovies }: AppProps) {
+  return (
+    <OverlayProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={<MovieHomePage initialMovies={initialMovies} />}
+        />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+      </Routes>
+    </OverlayProvider>
+  );
 }
 
 export default App;
