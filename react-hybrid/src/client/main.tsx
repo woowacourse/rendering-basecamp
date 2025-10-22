09f93bc0a06862b15657617c74d89bbfe14d7b9f
haku <hakukudh@gmail.com>
2025-10-22 15:18:25 +0900

    refactor: hydrate 수정

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { OverlayProvider } from 'overlay-kit';
+
+declare global {
+  interface Window {
+    __INITIAL_DATA__: any;
+    __INITIAL_URL__: string;
+  }
+}
 
 const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+console.log('initialData', initialData);
 
-hydrateRoot(document.getElementById("root"), <App />);
+hydrateRoot(
+  document.getElementById('root')!,
+  <OverlayProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </OverlayProvider>
+);
