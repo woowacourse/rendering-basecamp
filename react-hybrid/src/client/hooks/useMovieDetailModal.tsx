ff2a80e10ba1a3702ba3038ace645befc3aed767
deokbin <deokbin@naver.com>
2025-10-22 15:39:28 +0900

    feat: routing 처리

--- react-hybrid/src/client/hooks/useMovieDetailModal.tsx
+++ react-hybrid/src/client/hooks/useMovieDetailModal.tsx
@@ -1,16 +1,30 @@
-import { overlay } from 'overlay-kit';
-import { MovieDetailResponse } from '../types/MovieDetail.types';
-import { MovieDetailModal } from '../components/MovieDetailModal';
+import { overlay } from "overlay-kit";
+import { MovieDetailResponse } from "../types/MovieDetail.types";
+import { MovieDetailModal } from "../components/MovieDetailModal";
 
 export const useMovieDetailModal = () => {
+  const handleClose = () => {
+    window.history.pushState(null, "", "/");
+  };
+
   const openMovieDetailModal = (movie: MovieDetailResponse) => {
-    return new Promise<void>(resolve => {
+    window.history.pushState(null, "", `/detail/${movie.id}`);
+
+    const unlistenPopState = () => {
+      window.addEventListener("popstate", handleClose);
+    };
+
+    unlistenPopState();
+
+    return new Promise<void>((resolve) => {
       overlay.open(({ unmount }) => (
         <MovieDetailModal
           movie={movie}
           onClose={() => {
             resolve();
+            handleClose();
             unmount();
+            window.removeEventListener("popstate", handleClose);
           }}
         />
       ));
