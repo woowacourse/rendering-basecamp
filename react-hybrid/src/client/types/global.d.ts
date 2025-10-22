8d664de940b7da85fc42d83bd1ef6631b18eaea9
Oliy <eunsoa0310@gmail.com>
2025-10-22 13:38:41 +0900

    feat: global types에 영화 상세 응답 추가
    - Window 인터페이스에 detail 속성을 추가하여 MovieDetailResponse를 포함하도록 수정

--- react-hybrid/src/client/types/global.d.ts
+++ react-hybrid/src/client/types/global.d.ts
@@ -1,9 +1,11 @@
 import { MovieItem } from './Movie.types';
+import { MovieDetailResponse } from './MovieDetail.types';
 
 declare global {
   interface Window {
     __INITIAL_DATA__: {
       movies: MovieItem[];
+      detail: MovieDetailResponse;
     };
   }
 }
