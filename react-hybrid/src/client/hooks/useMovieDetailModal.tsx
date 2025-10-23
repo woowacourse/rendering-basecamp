87180174dd6f343c483d3a207be5287084ff58e8
eunwoo-levi <eunwoo1341@gmail.com>
2025-10-24 00:01:29 +0900

    feat: movie detail modal에 dynamic URL and meta 업데이트라는 로직 구현

--- react-hybrid/src/client/hooks/useMovieDetailModal.tsx
+++ react-hybrid/src/client/hooks/useMovieDetailModal.tsx
@@ -1,14 +1,28 @@
 import { overlay } from 'overlay-kit';
 import { MovieDetailResponse } from '../types/MovieDetail.types';
 import { MovieDetailModal } from '../components/MovieDetailModal';
+import { updatePageMeta, buildDetailPageMeta, buildHomePageMeta } from '../../server/utils/seoMeta';
 
 export const useMovieDetailModal = () => {
   const openMovieDetailModal = (movie: MovieDetailResponse) => {
     return new Promise<void>(resolve => {
+      if (typeof window !== "undefined") {
+        window.history.pushState({}, "", `/detail/${movie.id}`);
+      }
+      updatePageMeta(buildDetailPageMeta(movie));
+
       overlay.open(({ unmount }) => (
         <MovieDetailModal
           movie={movie}
           onClose={() => {
+            if (typeof window !== "undefined") {
+              window.history.replaceState({}, "", "/");
+            }
+            const initialMovies =
+              typeof window !== "undefined"
+                ? window.__INITIAL_DATA__?.movies
+                : undefined;
+            updatePageMeta(buildHomePageMeta(initialMovies));
             resolve();
             unmount();
           }}
