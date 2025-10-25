4f8ebed1281a09a3088bffa29178fd7253d9f506
hoyyChoi <hoyeon8165@gmail.com>
2025-10-25 23:14:04 +0900

    feat: 서버에서 데이터 로드 후, 넘겨받기

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import { MovieItem } from "./types/Movie.types";
 
-function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+function App({ movies }: { movies: MovieItem[] }) {
+  console.log(movies);
+  return <OverlayProvider>aa</OverlayProvider>;
 }
 
 export default App;
--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
-const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+const { movies } = window.__INITIAL_DATA__;
 
-hydrateRoot(document.getElementById("root"), <App />);
+hydrateRoot(document.getElementById("root"), <App movies={movies} />);
