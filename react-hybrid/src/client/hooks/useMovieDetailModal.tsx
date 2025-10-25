6b42523b8595abead96ced2af241900d157fca5f
mlnwns <mlnwns@naver.com>
2025-10-25 21:23:42 +0900

    fix: 영화 상세 모달의 상태 관리 로직 개선

--- react-hybrid/src/client/hooks/useMovieDetailModal.tsx
+++ react-hybrid/src/client/hooks/useMovieDetailModal.tsx
@@ -1,16 +1,19 @@
-import { overlay } from 'overlay-kit';
-import { MovieDetailResponse } from '../types/MovieDetail.types';
-import { MovieDetailModal } from '../components/MovieDetailModal';
+import { overlay } from "overlay-kit";
+import type { MovieDetailResponse } from "../types/MovieDetail.types";
+import { MovieDetailModal } from "../components/MovieDetailModal";
 
 export const useMovieDetailModal = () => {
   const openMovieDetailModal = (movie: MovieDetailResponse) => {
-    return new Promise<void>(resolve => {
+    window.history.pushState(null, "", `/detail/${movie.id}`);
+
+    return new Promise<void>((resolve) => {
       overlay.open(({ unmount }) => (
         <MovieDetailModal
           movie={movie}
           onClose={() => {
             resolve();
             unmount();
+            window.history.pushState(null, "", `/`);
           }}
         />
       ));
