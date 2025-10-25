0d461560f68b72ec86d3a3f314ce255cd35aca8b
hoyyChoi <hoyeon8165@gmail.com>
2025-10-25 23:40:00 +0900

    feat: 메인페이지 로드

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
 import { MovieItem } from "./types/Movie.types";
+import MovieHomePage from "./pages/MovieHomePage";
 
 function App({ movies }: { movies: MovieItem[] }) {
-  console.log(movies);
-  return <OverlayProvider>aa</OverlayProvider>;
+  return (
+    <OverlayProvider>
+      <MovieHomePage movies={movies} />
+    </OverlayProvider>
+  );
 }
 
 export default App;
