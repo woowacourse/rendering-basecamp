e6727ddcb93513aa72e8f6e943747b80ed2f5627
AHHYUNJU <wndkgus0225@naver.com>
2025-10-27 02:36:49 +0900

    feat: 초기에 서버에서 TMDB API를 호출하여 데이터를 먼저 얻도록 하는 로직 구현

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
-import { renderToString } from "react-dom/server";
-import App from "../../client/App";
-import React from "react";
+import { renderToString } from 'react-dom/server';
+import App from '../../client/App';
+import React from 'react';
+import { moviesApi } from '../../client/api/movies';
 
 const router = Router();
 
@@ -26,27 +27,67 @@ function generateHTML() {
     `;
 }
 
-router.get("/", (_: Request, res: Response) => {
-  const template = generateHTML();
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    const template = generateHTML();
 
-  const renderedApp = renderToString(<App />);
+    const moviesResponse = await moviesApi.getPopular(1);
+    const movies = moviesResponse.data.results;
 
-  const renderedHTMLWithInitialData = template.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    /*html*/ `
-    <script>
-      window.__INITIAL_DATA__ = {
-        movies: ${JSON.stringify([])}
-      }
-    </script>
-  `
-  );
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+    const renderedApp = renderToString(<App url={req.url} />);
 
-  res.send(renderedHTML);
+    const renderedHTMLWithInitialData = template.replace(
+      '<!--{INIT_DATA_AREA}-->',
+      /*html*/ `
+      <script>
+        window.__INITIAL_DATA__ = {
+          movies: ${JSON.stringify(movies)}
+        }
+      </script>
+    `
+    );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      '<!--{BODY_AREA}-->',
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/detail/:movieId', async (req: Request, res: Response) => {
+  try {
+    const template = generateHTML();
+    const movieId = parseInt(req.params.movieId, 10);
+
+    const movieDetailResponse = await moviesApi.getDetail(movieId);
+    const movieDetail = movieDetailResponse.data;
+
+    const renderedApp = renderToString(<App url={req.url} />);
+
+    const renderedHTMLWithInitialData = template.replace(
+      '<!--{INIT_DATA_AREA}-->',
+      /*html*/ `
+      <script>
+        window.__INITIAL_DATA__ = {
+          movieDetail: ${JSON.stringify(movieDetail)}
+        }
+      </script>
+    `
+    );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      '<!--{BODY_AREA}-->',
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error('Error fetching movie detail:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default router;
