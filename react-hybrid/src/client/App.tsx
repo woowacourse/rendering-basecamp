5856c91ca5ac1a9a421e885eeab9b4b9bdc263a4
Minje Sung <minje813@dgu.ac.kr>
2025-10-26 22:35:09 +0900

    feat: router가 가리키는 App에 MovieMainPage 표시

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { OverlayProvider } from "overlay-kit";
+import React from 'react';
+import { OverlayProvider } from 'overlay-kit';
+import MovieHomePage from './pages/MovieHomePage';
 
 function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+  return (
+    <OverlayProvider>
+      <MovieHomePage />
+    </OverlayProvider>
+  );
 }
 
 export default App;
