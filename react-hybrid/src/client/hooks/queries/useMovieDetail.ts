ceea32bc10ad367234be3d4dcd3db07ea7ceb10d
hanheel <hanhee630630@gmail.com>
2025-10-27 03:19:33 +0900

    feature: SSR 서버에서 HTML 반환값을 받아온 경우 해당 값을 기준으로 렌더링하도록 설정

--- react-hybrid/src/client/hooks/queries/useMovieDetail.ts
+++ react-hybrid/src/client/hooks/queries/useMovieDetail.ts
@@ -1,17 +1,34 @@
-import { useState, useEffect } from 'react';
-import { moviesApi } from '../../api/movies';
-import { MovieDetailResponse } from '../../types/MovieDetail.types';
+import { useState, useEffect } from "react";
+import { moviesApi } from "../../api/movies";
+import type { MovieDetailResponse } from "../../types/MovieDetail.types";
 
 /**
  * 영화 상세 정보를 조회하는 훅
  */
+
+const getInitialDetailData = () => {
+  if (
+    typeof window === "undefined" ||
+    !window.__INITIAL_DATA__ ||
+    !window.__INITIAL_DATA__.movieDetail
+  ) {
+    return null;
+  }
+  const initialData = window.__INITIAL_DATA__.movieDetail;
+  window.__INITIAL_DATA__.movieDetail = undefined;
+
+  return initialData;
+};
+
 export const useMovieDetail = (id: number) => {
-  const [data, setData] = useState<MovieDetailResponse | null>(null);
+  const [data, setData] = useState<MovieDetailResponse | null>(
+    () => getInitialDetailData() ?? null,
+  );
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    if (!id) return;
+    if (!id || data) return;
 
     const fetchMovieDetail = async () => {
       setIsLoading(true);
@@ -24,7 +41,7 @@ export const useMovieDetail = (id: number) => {
         setError(
           err instanceof Error
             ? err
-            : new Error('영화 상세 정보를 불러오는데 실패했습니다.')
+            : new Error("영화 상세 정보를 불러오는데 실패했습니다."),
         );
       } finally {
         setIsLoading(false);
@@ -32,7 +49,7 @@ export const useMovieDetail = (id: number) => {
     };
 
     fetchMovieDetail();
-  }, [id]);
+  }, [id, data]);
 
   return { data, isLoading, error };
 };
