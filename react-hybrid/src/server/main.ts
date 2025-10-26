51eda8e6ae2a204ca62c8202800cdfe7fc0320a4
H0ngJu <hongju4385@gmail.com>
2025-10-27 00:38:11 +0900

    chore: 서버 port 수정

--- react-hybrid/src/server/main.ts
+++ react-hybrid/src/server/main.ts
@@ -18,7 +18,6 @@ app.use("/images", express.static(path.join(__dirname, "../../public/images")));
 
 app.use("/", movieRouter);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
