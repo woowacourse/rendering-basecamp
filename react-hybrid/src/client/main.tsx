6f95f6752c42d1aa87251c6da0f498e7418bbb19
hoyyChoi <hoyeon8165@gmail.com>
2025-10-27 01:00:23 +0900

    chore: perfomance mark 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -4,7 +4,12 @@ import App from "./App";
 
 const { routeType, initialData } = window.__INITIAL_DATA__;
 
+performance.mark("beforeRender"); // Hydration 시작 시점 기록
+
 hydrateRoot(
   document.getElementById("root"),
   <App routeType={routeType} initialData={initialData} />
 );
+
+performance.mark("afterHydrate"); // Hydration 끝 시점 기록
+performance.measure("hydration", "beforeRender", "afterHydrate");
