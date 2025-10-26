9c0d1bdd140163d1ad480acc255c61d646c30cf1
H0ngJu <hongju4385@gmail.com>
2025-10-27 00:58:41 +0900

    chore: 이스터에그 추가

--- react-hybrid/src/client/components/MovieList.tsx
+++ react-hybrid/src/client/components/MovieList.tsx
@@ -18,7 +18,7 @@ export const MovieList = ({ movies }: { movies: MovieItemType[] }) => {
   return (
     <main>
       <section className="container">
-        <h2 className="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
+        <h2 className="text-2xl font-bold mb-64">🌼 데이지 하이 🌼</h2>
         <ul className="thumbnail-list">
           {movies.map(movie => (
             <MovieItem
