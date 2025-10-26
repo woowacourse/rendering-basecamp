2224c93df42fd5dd60e0e53126acbbc7e69aaa99
Jeongeun Lee <leeju1009@gmail.com>
2025-10-26 19:41:27 +0900

    feat: hydration 관련 로그 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -5,9 +5,16 @@ import App from "./App";
 
 const initialData = window.__INITIAL_DATA__;
 
+performance.mark("beforeRender");
+
 hydrateRoot(
   document.getElementById("root"),
   <OverlayProvider>
     <App movies={initialData?.movies} detailMovie={initialData?.detailMovie} />
   </OverlayProvider>
 );
+
+performance.mark("afterHydrate");
+performance.measure("hydration", "beforeRender", "afterHydrate");
+
+console.log("[Hydration] Completed");
