3dc27f1715a32dd49257f5dfdefab275c3036482
ohgus <anakin1108@naver.com>
2025-10-27 01:11:44 +0900

    refactor: movieDetail이 있을 경우 상세 페이지 렌더링

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { OverlayProvider } from 'overlay-kit';
 import MovieHomePage from './pages/MovieHomePage';
 import { MovieItem } from './types/Movie.types';
+import MovieDetailPage from './pages/MovieDetailPage';
+import { MovieDetailResponse } from './types/MovieDetail.types';
 
 interface AppProps {
-  movies: MovieItem[];
+  movies?: MovieItem[];
+  movieDetail?: MovieDetailResponse;
 }
 
-function App({ movies }: AppProps) {
+function App({ movies, movieDetail }: AppProps) {
+  const isDetailPage = movieDetail != null;
+
+  useEffect(() => {
+    const handlePopState = () => {
+      window.location.reload();
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
   return (
     <OverlayProvider>
-      <MovieHomePage movies={movies} />
+      {isDetailPage ? (
+        <MovieDetailPage movies={movies} movieDetail={movieDetail} />
+      ) : (
+        <MovieHomePage movies={movies} />
+      )}
     </OverlayProvider>
   );
 }
