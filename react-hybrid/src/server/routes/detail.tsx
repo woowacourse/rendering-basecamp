e6a6071da38d8bb32c0929ec7777bd4a5d991e55
mun-kyeong <horse6953@naver.com>
2025-10-27 01:37:31 +0900

    fix: 오류 처리 추가 및 코드 구조 개선

--- react-hybrid/src/server/routes/detail.tsx
+++ react-hybrid/src/server/routes/detail.tsx
@@ -10,37 +10,38 @@ import generateHTML from "../utils/generateHTML";
 const router = Router();
 
 router.get("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const template = generateHTML();
+    const template = generateHTML();
 
-  const [movieList, movieDetail] = await Promise.all([
-    moviesApi.getPopular(),
-    moviesApi.getDetail(Number(id)),
-  ]);
+    const [movieList, movieDetail] = await Promise.all([
+      moviesApi.getPopular(),
+      moviesApi.getDetail(Number(id)),
+    ]);
 
-  const renderedApp = renderToString(
-    <App
-      initialMovies={{
-        movies: movieList.data.results,
-        details: movieDetail.data,
-      }}
-    />
-  );
+    const renderedApp = renderToString(
+      <App
+        initialMovies={{
+          movies: movieList.data.results,
+          details: movieDetail.data,
+        }}
+      />
+    );
 
-  const renderedHTMLWithSeo = template.replace(
-    "<!--{OG_TAGS}-->",
-    SeoHead({
-      title: movieDetail.data.title,
-      description: movieDetail.data.overview,
-      image: `https://image.tmdb.org/t/p/w1280${movieDetail.data.backdrop_path}`,
-      url: `https://rendering-basecamp-production-8f18.up.railway.app/detail/${movieDetail.data.id}`,
-    })
-  );
+    const renderedHTMLWithSeo = template.replace(
+      "<!--{OG_TAGS}-->",
+      SeoHead({
+        title: movieDetail.data.title,
+        description: movieDetail.data.overview,
+        image: `https://image.tmdb.org/t/p/w1280${movieDetail.data.backdrop_path}`,
+        url: `https://rendering-basecamp-production-8f18.up.railway.app/detail/${movieDetail.data.id}`,
+      })
+    );
 
-  const renderedHTMLWithInitialData = renderedHTMLWithSeo.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    `
+    const renderedHTMLWithInitialData = renderedHTMLWithSeo.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      `
     <script>
       window.__INITIAL_DATA__ = {
         movies: ${JSON.stringify(movieList.data.results)},
@@ -48,14 +49,17 @@ router.get("/:id", async (req: Request, res: Response) => {
       }
     </script>
   `
-  );
+    );
 
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
 
-  res.send(renderedHTML);
+    res.send(renderedHTML);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -10,41 +10,46 @@ import generateHTML from "../utils/generateHTML";
 const router = Router();
 
 router.get("/", async (_: Request, res: Response) => {
-  const template = generateHTML();
+  try {
+    const movieList = await moviesApi.getPopular();
+    console.log(movieList);
 
-  const movieList = await moviesApi.getPopular();
+    const renderedApp = renderToString(
+      <App initialMovies={{ movies: movieList.data.results }} />
+    );
 
-  const renderedApp = renderToString(
-    <App initialMovies={{ movies: movieList.data.results }} />
-  );
+    const template = generateHTML();
 
-  const renderedHTMLWithSeo = template.replace(
-    "<!--{OG_TAGS}-->",
-    SeoHead({
-      title: "인기 영화 추천",
-      description: "지금 인기 있는 영화들을 만나보세요.",
-      image: `https://image.tmdb.org/t/p/w1280${movieList.data.results[0].backdrop_path}`,
-      url: "https://rendering-basecamp-production-8f18.up.railway.app/",
-    })
-  );
+    const renderedHTMLWithSeo = template.replace(
+      "<!--{OG_TAGS}-->",
+      SeoHead({
+        title: "인기 영화 추천",
+        description: "지금 인기 있는 영화들을 만나보세요.",
+        image: `https://image.tmdb.org/t/p/w1280${movieList.data.results[0].backdrop_path}`,
+        url: "https://rendering-basecamp-production-8f18.up.railway.app/",
+      })
+    );
 
-  const renderedHTMLWithInitialData = renderedHTMLWithSeo.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    `
+    const renderedHTMLWithInitialData = renderedHTMLWithSeo.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      `
     <script>
       window.__INITIAL_DATA__ = {
         movies: ${JSON.stringify(movieList.data.results)}
       }
     </script>
   `
-  );
+    );
 
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+    const renderedHTML = renderedHTMLWithInitialData.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
 
-  res.send(renderedHTML);
+    res.send(renderedHTML);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
