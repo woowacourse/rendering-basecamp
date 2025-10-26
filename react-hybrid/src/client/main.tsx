5da5b33755f997faeb6443fd2c59dd652aca7c34
AHHYUNJU <wndkgus0225@naver.com>
2025-10-27 04:32:49 +0900

    fix: dotenv 제거

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import App from './App';
--- react-hybrid/webpack.client.config.js
+++ react-hybrid/webpack.client.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -20,7 +19,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: [
-              "@babel/preset-env", 
+              "@babel/preset-env",
               ["@babel/preset-react", { "runtime": "automatic" }],
               ["@babel/preset-typescript", { "isTSX": true, "allExtensions": true }]
             ],
@@ -47,7 +46,6 @@ module.exports = {
         { from: "public/styles", to: "styles" },
       ],
     }),
-    new Dotenv(),
   ],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
