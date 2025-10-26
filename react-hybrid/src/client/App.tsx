87007515e4269e64f101904202e49c1476b52116
bunju20 <ghkd2271@naver.com>
2025-10-27 08:47:34 +0900

    feat: SSR 페이지 렌더링 함수 구현

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import type { MovieItem } from "./types/Movie.types";
+import type { MovieDetailResponse } from "./types/MovieDetail.types";
 
-function App() {
+function App({
+  routeType,
+  initialData,
+}: {
+  routeType: "home" | "detail";
+  initialData: {
+    movies: MovieItem[];
+    selectedMovieDetail?: MovieDetailResponse;
+  };
+}) {
   return <OverlayProvider>App</OverlayProvider>;
 }
 
