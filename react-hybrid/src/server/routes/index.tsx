8ca1fcaed02a9899a63c3422901900aa1ee51671
jin123457 <jinsol9909@gmail.com>
2025-10-27 07:31:33 +0900

    feat: 영화 상세 페이지에 OG 태그 생성 기능 추가 및 초기 데이터에 영화 상세 정보 포함

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -3,6 +3,7 @@ import { Router, type Request, type Response } from "express";
 import { renderToString } from "react-dom/server";
 import App from "../../client/App";
 import { moviesApi } from "../../client/api/movies";
+import { generateOGTags } from "../utils/generateOGTags";
 
 const router = Router();
 
@@ -26,28 +27,98 @@ function generateHTML() {
     `;
 }
 
-router.get("/", async (_: Request, res: Response) => {
-  const template = generateHTML();
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const template = generateHTML();
 
-  const popularMoviesResponse = await moviesApi.getPopular();
-  const movies = popularMoviesResponse.data.results ?? [];
-  const renderedApp = renderToString(<App initialMovies={movies} />);
-  const renderedHTMLWithInitialData = template.replace(
-    "<!--{INIT_DATA_AREA}-->",
-    /*html*/ `
+    const popularMoviesResponse = await moviesApi.getPopular();
+    const movies = popularMoviesResponse.data.results ?? [];
+    const renderedApp = renderToString(<App initialMovies={movies} />);
+
+    const renderedHTMLWithInitialData = template.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      /*html*/ `
     <script>
       window.__INITIAL_DATA__ = {
         movies: ${JSON.stringify(movies ?? [])}
       }
     </script>
   `
-  );
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+    );
 
-  res.send(renderedHTML);
+    const ogTags = generateOGTags({
+      title: "세오의 추천 영화",
+      description: "세오가 추천하는 인기 영화들을 확인하세요",
+      url: `https://attractive-embrace-production.up.railway.app/`,
+      image: `https://image.tmdb.org/t/p/w500${movies[0].poster_path || ""}`,
+    });
+
+    const renderedHTMLWithOG = renderedHTMLWithInitialData.replace(
+      "<!--{OG_TAGS}-->",
+      ogTags
+    );
+
+    const renderedHTML = renderedHTMLWithOG.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error("Error during server-side rendering:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/detail:id", async (req: Request, res: Response) => {
+  try {
+    const template = generateHTML();
+    const { id } = req.params;
+    const [popularMoviesResponse, popularMovieDetailResponse] =
+      await Promise.all([
+        moviesApi.getPopular(),
+        moviesApi.getDetail(Number(id)),
+      ]);
+    const popularMovies = Array.isArray(popularMoviesResponse?.data.results)
+      ? popularMoviesResponse.data.results
+      : [];
+    const movieDetail = popularMovieDetailResponse.data ?? null;
+    const renderedApp = renderToString(<App initialMovies={popularMovies} />);
+
+    const ogTags = generateOGTags({
+      title: `${movieDetail?.title || "영화 상세"}`,
+      description: movieDetail?.overview || "영화 상세 정보를 확인하세요",
+      url: `https://rendering-basecamp-production-f101.up.railway.app/detail/${id}`,
+      image: `https://image.tmdb.org/t/p/w500${movieDetail?.poster_path || ""}`,
+    });
+
+    const renderedHTMLWithInitialData = template.replace(
+      "<!--{INIT_DATA_AREA}-->",
+      /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = {
+        movies: ${JSON.stringify(popularMovies ?? [])}
+        detail: ${JSON.stringify(movieDetail)}
+      }
+    </script>
+  `
+    );
+
+    const renderedHTMLWithOG = renderedHTMLWithInitialData.replace(
+      "<!--{OG_TAGS}-->",
+      ogTags
+    );
+
+    const renderedHTML = renderedHTMLWithOG.replace(
+      "<!--{BODY_AREA}-->",
+      renderedApp
+    );
+
+    res.send(renderedHTML);
+  } catch (error) {
+    console.error("Error during server-side rendering:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
