4eb189c651dd6ac7e30f721b73d0faf5f8d34960
haku <hakukudh@gmail.com>
2025-10-26 22:13:24 +0900

    feat: 퍼포먼스 로그 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -13,9 +13,17 @@ declare global {
 const initialData = window.__INITIAL_DATA__;
 console.log('initialData', initialData);
 
+performance.mark('beforeRender');
+
 hydrateRoot(
   document.getElementById('root')!,
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+performance.mark('afterHydrate');
+performance.measure('hydration', 'beforeRender', 'afterHydrate');
+
+const measure = performance.getEntriesByName('hydration')[0];
+console.log('Hydration 소요시간:', measure.duration.toFixed(2), 'ms');
