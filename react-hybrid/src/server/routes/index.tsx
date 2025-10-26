cb41587610f2bd3d9f8d0be78d46a3a08c9b3322
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-26 20:43:28 +0900

    feat: 초기 영화 데이터를 App 컴포넌트에 전달하도록 수정

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
-import { renderToString } from "react-dom/server";
-import App from "../../client/App";
-import React from "react";
+import { renderToString } from 'react-dom/server';
+import App from '../../client/App';
+import React from 'react';
+import { moviesApi } from '../../client/api/movies';
 
 const router = Router();
 
@@ -26,25 +27,23 @@ function generateHTML() {
     `;
 }
 
-router.get("/", (_: Request, res: Response) => {
+router.get('/', async (_: Request, res: Response) => {
+  const movies = await moviesApi.getPopular();
   const template = generateHTML();
 
-  const renderedApp = renderToString(<App />);
+  const renderedApp = renderToString(<App initialMovies={movies.data.results} />);
 
   const renderedHTMLWithInitialData = template.replace(
-    "<!--{INIT_DATA_AREA}-->",
+    '<!--{INIT_DATA_AREA}-->',
     /*html*/ `
     <script>
       window.__INITIAL_DATA__ = {
-        movies: ${JSON.stringify([])}
+         movies: ${JSON.stringify(movies.data.results)},
       }
     </script>
-  `
+  `,
   );
-  const renderedHTML = renderedHTMLWithInitialData.replace(
-    "<!--{BODY_AREA}-->",
-    renderedApp
-  );
+  const renderedHTML = renderedHTMLWithInitialData.replace('<!--{BODY_AREA}-->', renderedApp);
 
   res.send(renderedHTML);
 });
