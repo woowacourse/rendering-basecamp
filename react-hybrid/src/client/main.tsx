03244786f717972868daa6fe740d755f493b5ead
ohgus <anakin1108@naver.com>
2025-10-27 01:12:02 +0900

    feat: movieDetail 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -3,6 +3,10 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 
 const initialData = window.__INITIAL_DATA__;
-const movies = initialData.movies;
+const movies = initialData?.movies;
+const movieDetail = initialData?.movieDetail;
 
-hydrateRoot(document.getElementById('root'), <App movies={movies} />);
+hydrateRoot(
+  document.getElementById('root'),
+  <App movies={movies} movieDetail={movieDetail} />
+);
