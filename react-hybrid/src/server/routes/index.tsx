d1e7818d9c7573c932d83a1f310b3372e240839e
Beomtae <region9@naver.com>
2025-10-26 12:20:39 +0900

    feat: og 태그 적용

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -7,6 +7,14 @@ import { moviesApi } from "../../client/api/movies";
 
 const router = Router();
 
+interface OgTagsParams {
+  title: string;
+  description: string;
+  url: string;
+  imageUrl: string;
+  website: string;
+}
+
 function generateHTML() {
   return /*html*/ `
     <!DOCTYPE html>
@@ -27,18 +35,94 @@ function generateHTML() {
     `;
 }
 
-router.get("/", async (_: Request, res: Response) => {
+function generateOgTags({
+  title,
+  description,
+  url,
+  imageUrl,
+  website,
+}: OgTagsParams) {
+  return /*html*/ `
+    <meta property="og:title" content="${title}" />
+    <meta property="og:description" content="${description}" />
+    <meta property="og:image" content="${imageUrl}" />
+    <meta property="og:type" content="website" />
+  `;
+}
+
+router.get("/", async (req: Request, res: Response) => {
   const template = generateHTML();
   const popularMoviesResponse = await moviesApi.getPopular();
   const movies = popularMoviesResponse.data.results;
 
+  const baseUrl = `${req.protocol}://${req.get("host")}`;
+  const ogImage = movies?.[0]?.backdrop_path
+    ? `https://image.tmdb.org/t/p/w1280${movies[0].backdrop_path}`
+    : `${baseUrl}/images/logo.png`;
+
   const renderedApp = renderToString(<App initialMovies={movies} />);
 
-  const renderedHTMLWithInitialData = template.replace(
+  const renderedHTMLWithOgTags = template.replace(
+    "<!--{OG_TAGS}-->",
+    generateOgTags({
+      title: "영화 리뷰 - 인기 영화",
+      description: "가장 인기 있는 영화들을 만나보세요.",
+      url: `${baseUrl}/`,
+      imageUrl: `${ogImage}`,
+      website: "영화 리뷰",
+    })
+  );
+
+  const renderedHTMLWithInitialData = renderedHTMLWithOgTags.replace(
     "<!--{INIT_DATA_AREA}-->",
     /*html*/ `
     <script>
       window.__INITIAL_DATA__ = ${JSON.stringify(movies)}
+      
+    </script>
+  `
+  );
+  const renderedHTML = renderedHTMLWithInitialData.replace(
+    "<!--{BODY_AREA}-->",
+    renderedApp
+  );
+
+  res.send(renderedHTML);
+});
+
+router.get("/detail/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const template = generateHTML();
+
+  const [popularMoviesResponse, detailMovieResponse] = await Promise.all([
+    moviesApi.getPopular(),
+    moviesApi.getDetail(Number(id)),
+  ]);
+
+  const movies = popularMoviesResponse.data.results;
+  const detailMovie = detailMovieResponse.data;
+
+  const baseUrl = `${req.protocol}://${req.get("host")}`;
+  const ogImage = `https://image.tmdb.org/t/p/w1280${detailMovie.poster_path}`;
+
+  const renderedApp = renderToString(<App initialMovies={movies} />);
+
+  const renderedHTMLWithOgTags = template.replace(
+    "<!--{OG_TAGS}-->",
+    generateOgTags({
+      title: `영화 리뷰 - ${detailMovie.title}`,
+      description: `${detailMovie.overview}`,
+      url: `${baseUrl}/`,
+      imageUrl: `${ogImage}`,
+      website: "영화 리뷰",
+    })
+  );
+
+  const renderedHTMLWithInitialData = renderedHTMLWithOgTags.replace(
+    "<!--{INIT_DATA_AREA}-->",
+    /*html*/ `
+    <script>
+      window.__INITIAL_DATA__ = ${JSON.stringify({ movies, detailMovie })}
       
     </script>
   `
