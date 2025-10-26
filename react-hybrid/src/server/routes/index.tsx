f06923a8526e1efac0a40da7cf63a5e162d0a9b0
haku <hakukudh@gmail.com>
2025-10-26 22:40:21 +0900

    fix: 잘못된 내용 수정

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -42,7 +42,7 @@ function generateHTML(
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles/index.css" />
-        <title>${ogTitle}</title>
+        <title>영화 리뷰</title>
 
         <!-- Open Graph 태그 -->
         <meta property="og:type" content="website" />
