bcbe83ef826faeaadb9b03fcef63a509753a4eb8
jeongyou <wjddbwjd1205@naver.com>
2025-10-27 00:44:52 +0900

    fix: 배포 환경에서 환경변수가 로드되지 않는 문제 수정
    - dotenv-webpack 설정에 systemvars 옵션 추가

--- react-hybrid/webpack.client.config.js
+++ react-hybrid/webpack.client.config.js
@@ -20,7 +20,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: [
-              "@babel/preset-env", 
+              "@babel/preset-env",
               ["@babel/preset-react", { "runtime": "automatic" }],
               ["@babel/preset-typescript", { "isTSX": true, "allExtensions": true }]
             ],
@@ -47,7 +47,9 @@ module.exports = {
         { from: "public/styles", to: "styles" },
       ],
     }),
-    new Dotenv(),
+    new Dotenv({
+      systemvars: true,
+    }),
   ],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
