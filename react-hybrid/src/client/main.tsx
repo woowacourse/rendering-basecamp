cdba19db624b5932c34d1feefcaba0285f4bcaf1
mun-kyeong <horse6953@naver.com>
2025-10-27 02:17:04 +0900

    fix: 초기 영화 데이터 구조 수정

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
-const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+const initialMovies = window.__INITIAL_DATA__
+  ? {
+      movies: window.__INITIAL_DATA__.movies,
+      details: window.__INITIAL_DATA__.detail ?? null,
+    }
+  : undefined;
 
 hydrateRoot(
   document.getElementById("root"),
-  <App initialMovies={initialData.movies} />
+  <App initialMovies={initialMovies} />
 );
