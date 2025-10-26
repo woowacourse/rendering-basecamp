ad5866ead468de2adef018a68705d51d559bbaeb
SuHwa Lee <tnghk9611@naver.com>
2025-10-26 22:55:49 +0900

    chore: 로그 추가

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -3,9 +3,18 @@ import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 
 const initialData = window.__INITIAL_DATA__;
-console.log("initialData", initialData);
+
+performance.mark("hydration-start");
 
 hydrateRoot(
   document.getElementById("root"),
   <App initialMovies={initialData?.movies} />
 );
+
+requestIdleCallback(() => {
+  performance.mark("hydration-end");
+  performance.measure("hydration", "hydration-start", "hydration-end");
+
+  const measure = performance.getEntriesByName("hydration")[0];
+  console.log(`Hydration took ${measure.duration}ms`);
+});
