e8b1ce6b844351fb803187b4eeea4d36f6e7369e
마 <126178440+shuyeon@users.noreply.github.com>
2025-10-27 02:16:41 +0900

    feat: 영화 상세 페이지 이동 onClick=>a태그

--- react-hybrid/src/client/components/MovieItem.tsx
+++ react-hybrid/src/client/components/MovieItem.tsx
@@ -2,38 +2,31 @@ import type { MovieItem as MovieItemType } from "../types/Movie.types";
 
 interface MovieItemProps {
   movie: MovieItemType;
-  onClick: (movie: MovieItemType) => void;
-  ref?: React.Ref<HTMLLIElement>;
 }
 
-export const MovieItem = ({ movie, onClick, ref }: MovieItemProps) => {
-  const { title, poster_path, vote_average } = movie;
+export const MovieItem = ({ movie }: MovieItemProps) => {
+  const { title, poster_path, vote_average, id } = movie;
 
   const imageUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
     : "/static/images/no_image.png";
 
-  const handleClick = () => {
-    onClick(movie);
-  };
-
   return (
-    <li
-      ref={ref}
-      className="movie-item"
-      onClick={handleClick}
-      data-index={movie.id}
-    >
-      <div className="item">
+    <li className="movie-item" data-index={id}>
+      <a
+        href={`/detail/${id}`}
+        className="item"
+        aria-label={`${title} 상세 페이지로 이동`}
+      >
         <img className="thumbnail" src={imageUrl} alt={title} loading="lazy" />
         <div className="item-desc">
           <p className="rate">
-            <img src="/static/images/star_empty.png" className="star" />
+            <img src="/static/images/star_empty.png" className="star" alt="" />
             <span>{vote_average.toFixed(1)}</span>
           </p>
           <strong>{title}</strong>
         </div>
-      </div>
+      </a>
     </li>
   );
 };
--- react-hybrid/src/client/components/MovieList.tsx
+++ react-hybrid/src/client/components/MovieList.tsx
@@ -1,24 +1,14 @@
-import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
 import { MovieItem } from "./MovieItem";
 import type { MovieItem as MovieItemType } from "../types/Movie.types";
-import { moviesApi } from "../api/movies";
 
 export const MovieList = ({ movies }: { movies: MovieItemType[] }) => {
-  const handleMovieClick = async (movie: MovieItemType) => {
-    window.history.pushState({}, "", `/detail/${movie.id}`);
-  };
-
   return (
     <main>
       <section className="container">
         <h2 className="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
         <ul className="thumbnail-list">
           {movies.map((movie) => (
-            <MovieItem
-              key={movie.id}
-              movie={movie}
-              onClick={handleMovieClick}
-            />
+            <MovieItem key={movie.id} movie={movie} />
           ))}
         </ul>
       </section>
