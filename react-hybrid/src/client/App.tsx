bff791f927b13af2ad0cbd998c618fe1f298e4c3
이상희 <sang6299@naver.com>
2025-10-26 17:49:32 +0900

    feat: App 컴포넌트에 라우팅 추가

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MovieHomePage from "./pages/MovieHomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MovieHomePage />,
+  },
+  {
+    path: "/detail/:movieId",
+    element: <MovieDetailPage />,
+  },
+]);
 
 function App() {
-  return <OverlayProvider>App</OverlayProvider>;
+  return (
+    <OverlayProvider>
+      <RouterProvider router={router} />
+    </OverlayProvider>
+  );
 }
 
 export default App;
