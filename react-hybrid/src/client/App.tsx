94e4bc3834a6c63371764c641ee1ab7c0b3c814a
bunju20 <ghkd2271@naver.com>
2025-10-27 08:53:11 +0900

    refactor: App 컴포넌트를 SSR/CSR 모두 지원하도록 수정

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,19 +1,31 @@
-import React from "react";
-import { OverlayProvider } from "overlay-kit";
-import type { MovieItem } from "./types/Movie.types";
-import type { MovieDetailResponse } from "./types/MovieDetail.types";
+import { OverlayProvider } from "./contexts/OverlayContext";
+import { MovieHomePage } from "./pages/MovieHomePage";
+import { MovieDetailPage } from "./pages/MovieDetailPage";
+import { MovieItem } from "./types/Movie.types";
+import { MovieDetailResponse } from "./types/MovieDetail.types";
 
-function App({
-  routeType,
-  initialData,
-}: {
+interface AppProps {
   routeType: "home" | "detail";
   initialData: {
     movies: MovieItem[];
     selectedMovieDetail?: MovieDetailResponse;
   };
-}) {
-  return <OverlayProvider>App</OverlayProvider>;
 }
+
+const App = ({ routeType, initialData }: AppProps) => {
+  const { movies, selectedMovieDetail } = initialData;
+
+  return (
+    <OverlayProvider>
+      {routeType === "home" && <MovieHomePage initialMovies={movies} />}
+      {routeType === "detail" && (
+        <MovieDetailPage
+          initialMovies={movies}
+          selectedMovieDetail={selectedMovieDetail}
+        />
+      )}
+    </OverlayProvider>
+  );
+};
 
 export default App;
--- react-hybrid/src/client/pages/MovieHomePage.tsx
+++ react-hybrid/src/client/pages/MovieHomePage.tsx
@@ -1,25 +1,30 @@
-import { Header } from '../components/Header';
-import { MovieList } from '../components/MovieList';
-import { Footer } from '../components/Footer';
-import { usePopularMovies } from '../hooks/queries/usePopularMovies';
-import { Loading } from '../components/common/Loading';
+import { Header } from "../components/Header";
+import { MovieList } from "../components/MovieList";
+import { Footer } from "../components/Footer";
+import { usePopularMovies } from "../hooks/queries/usePopularMovies";
+import { Loading } from "../components/common/Loading";
+import { MovieItem } from "../types/Movie.types";
 
-export default function MovieHomePage() {
-  const { data: movies, isLoading } = usePopularMovies();
+interface MovieHomePageProps {
+  initialMovies: MovieItem[];
+}
+
+export const MovieHomePage = ({ initialMovies }: MovieHomePageProps) => {
+  const { data: popularMovies, isLoading } = usePopularMovies();
 
   if (isLoading === true) {
     return <Loading />;
   }
 
-  if (movies == null || movies.length === 0) {
+  if (popularMovies == null || popularMovies.length === 0) {
     return <div>영화 정보를 불러오는데 실패했습니다.</div>;
   }
 
   return (
     <div id="wrap">
-      <Header featuredMovie={movies[0]} />
-      <MovieList movies={movies} />
+      <Header featuredMovie={popularMovies[0]} />
+      <MovieList movies={popularMovies} />
       <Footer />
     </div>
   );
-}
+};
