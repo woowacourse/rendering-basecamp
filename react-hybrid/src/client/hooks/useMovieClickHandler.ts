2d9afe50f198b4ee282f3c0dd47a1ecae6070e86
bunju20 <ghkd2271@naver.com>
2025-10-27 08:55:40 +0900

    feat: 클라이언트 라우팅 및 정적 파일 경로 수정

---
+++ react-hybrid/src/client/hooks/useMovieClickHandler.ts
@@ -0,0 +1,16 @@
+import { useMovieDetailModal } from "./useMovieDetailModal";
+import { moviesApi } from "../api/movies";
+import { MovieItem } from "../types/Movie.types";
+
+export const useMovieClickHandler = () => {
+  const { openMovieDetailModal } = useMovieDetailModal();
+
+  const handleMovieClick = async (movie: MovieItem) => {
+    window.history.pushState(null, "", `/detail/${movie.id}`);
+
+    const movieDetail = await moviesApi.getDetail(movie.id);
+    await openMovieDetailModal(movieDetail.data);
+  };
+
+  return { handleMovieClick };
+};
