c3a5be38008e5021d9707f21e73eb4f7e7749777
yeongipark <yeongi0111@naver.com>
2025-10-27 22:28:29 +0900

    fix: og 짤리는 문제 해결

--- react-hybrid/src/client/utils/createSEO.ts
+++ react-hybrid/src/client/utils/createSEO.ts
@@ -23,13 +23,12 @@ export function createSEO({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>${title}</title>
-        <meta name="description" content=${description} />
-
-        <meta property="og:type" content=${ogType} />
-        <meta property="og:title" content=${ogTitle} />
-        <meta property="og:description" content=${ogDescription} />
-        <meta property="og:image" content=${ogImage} />
-        <meta property="og:url" content=${ogUrl} />
+        <meta name="description" content="${description}" />
+        <meta property="og:type" content="${ogType}" />
+        <meta property="og:title" content="${ogTitle}" />
+        <meta property="og:description" content="${ogDescription}" />
+        <meta property="og:image" content="${ogImage}" />
+        <meta property="og:url" content="${ogUrl}" />
       </head>
   `;
 }
