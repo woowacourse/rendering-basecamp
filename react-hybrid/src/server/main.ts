2393dd1f01efbea0d3d45f45d6bcf802401a7a0e
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-27 09:25:59 +0900

    fix: 정적 파일 경로를 절대 경로로 수정

--- react-hybrid/src/server/main.ts
+++ react-hybrid/src/server/main.ts
@@ -9,12 +9,12 @@ import movieRouter from "./routes/index";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
+const __dirname = path.resolve("dist/server");
 
 // 정적 파일 제공
-app.use("/static", express.static(path.join(__dirname, "../static")));
-app.use("/images", express.static(path.join(__dirname, "../../public/images")));
-app.use("/styles", express.static(path.join(__dirname, "../../public/styles")));
+app.use("/static", express.static(path.resolve("dist/static")));
+app.use("/images", express.static(path.resolve("public/images")));
+app.use("/styles", express.static(path.resolve("public/styles")));
 
 app.use("/", movieRouter);
 
