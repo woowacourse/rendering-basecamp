fbd05bab521b8e7de84c76b8b03e416e999aa02f
eunwoo-levi <eunwoo1341@gmail.com>
2025-10-28 00:01:46 +0900

    feat: detail 페이지로 갈 경우 모달 켜는 로직 추가

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { OverlayProvider } from "overlay-kit";
 import { MovieItem } from './types/Movie.types';
+import { MovieDetailResponse } from './types/MovieDetail.types';
 import MovieHomePage from './pages/MovieHomePage';
+import { useMovieDetailModal } from './hooks/useMovieDetailModal';
 
 interface AppProps {
   initialMovies: MovieItem[];
 }
 
+function AppContent({ initialMovies }: AppProps) {
+  const { openMovieDetailModal } = useMovieDetailModal();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const initialData = (window as any).__INITIAL_DATA__;
+    const detail: MovieDetailResponse | undefined = initialData?.detail;
+
+    if (detail) {
+      openMovieDetailModal(detail);
+    }
+  }, []);
+
+  return <MovieHomePage initialMovies={initialMovies} />;
+}
+
 function App({ initialMovies }: AppProps) {
   return (
     <OverlayProvider>
-      <MovieHomePage initialMovies={initialMovies} />
+      <AppContent initialMovies={initialMovies} />
     </OverlayProvider>
   );
 }
