e13e1c78daa97d1b72c58d458817c191125fcf38
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-27 09:12:45 +0900

    feat: React Router를 사용한 라우팅 구현

--- react-hybrid/src/client/App.tsx
+++ react-hybrid/src/client/App.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { OverlayProvider } from "overlay-kit";
+import { Routes, Route } from "react-router-dom";
 import MovieHomePage from "./pages/MovieHomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import { MovieItem } from "./types/Movie.types";
+import { MovieDetailResponse } from "./types/MovieDetail.types";
 
-function App() {
+function App({
+  initialMovies,
+  initialDetail,
+}: {
+  initialMovies: MovieItem[];
+  initialDetail?: MovieDetailResponse;
+}) {
   return (
     <OverlayProvider>
-      <MovieHomePage />
+      <Routes>
+        <Route path="/" element={<MovieHomePage movies={initialMovies} />} />
+        <Route
+          path="/detail/:movieId"
+          element={
+            <MovieDetailPage
+              movies={initialMovies}
+              initialDetail={initialDetail}
+            />
+          }
+        />
+      </Routes>
     </OverlayProvider>
   );
 }
