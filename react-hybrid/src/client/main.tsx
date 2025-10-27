22acfbef436d775f7f26d64f678c5728dc6e624b
mun-kyeong <horse6953@naver.com>
2025-10-27 12:23:18 +0900

    fix: WebSocket 임포트 추가 및 성능 측정 마크업 개선

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
+import { WebSocket } from "http";
 
 const initialMovies = window.__INITIAL_DATA__
   ? {
@@ -9,7 +10,12 @@ const initialMovies = window.__INITIAL_DATA__
     }
   : undefined;
 
+performance.mark("hydration-start");
+
 hydrateRoot(
   document.getElementById("root"),
   <App initialMovies={initialMovies} />
 );
+
+performance.mark("hydration-end");
+performance.measure("hydration", "hydration-start", "hydration-end");
