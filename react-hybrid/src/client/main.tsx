0c71a273b9964b58c0632a795b87fe3b78dbd962
Oliy <eunsoa0310@gmail.com>
2025-10-27 09:18:44 +0900

    chore: 사용하지 않는 console log 삭제

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -3,7 +3,6 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 
 const initialData = window.__INITIAL_DATA__;
-console.log('initialData', initialData);
 
 hydrateRoot(
   document.getElementById('root'),
