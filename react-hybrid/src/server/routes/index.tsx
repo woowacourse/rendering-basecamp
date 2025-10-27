ef9553a3444e9ba26b13fb85d3135010c92d464d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-27 09:54:14 +0900

    feat: getRequestUrl 유틸리티 추가 및 OG 태그에 현재 URL 적용

--- react-hybrid/src/server/routes/index.tsx
+++ react-hybrid/src/server/routes/index.tsx
@@ -5,6 +5,7 @@ import App from '../../client/App';
 import React from 'react';
 import { moviesApi } from '../../client/api/movies';
 import { generateOGTags } from '../utils/generateOGTags';
+import { getRequestUrl } from '../utils/getRequestUrl';
 
 const router = Router();
 
@@ -28,7 +29,7 @@ function generateHTML() {
     `;
 }
 
-router.get('/', async (_: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
   const movies = await moviesApi.getPopular();
   const template = generateHTML();
 
@@ -39,10 +40,12 @@ router.get('/', async (_: Request, res: Response) => {
     />,
   );
 
+  const currentUrl = getRequestUrl(req);
+
   const ogTags = generateOGTags({
     title: '영화 리뷰 - 지금 인기 있는 영화',
     description: '최신 인기 영화를 확인하고 리뷰를 남겨보세요',
-    url: 'http://localhost:3000/',
+    url: currentUrl,
     type: 'website',
     siteName: '영화 리뷰',
   });
@@ -82,11 +85,13 @@ router.get('/detail/:id', async (req: Request, res: Response) => {
     ? `https://image.tmdb.org/t/p/w500${movieDetail.data.poster_path}`
     : undefined;
 
+  const currentUrl = getRequestUrl(req);
+
   const ogTags = generateOGTags({
     title: movieDetail.data.title,
     description: movieDetail.data.overview || movieDetail.data.title,
     image: posterUrl,
-    url: `http://localhost:3000/detail/${movieId}`,
+    url: currentUrl,
     type: 'video.movie',
     siteName: '영화 상세 정보',
   });
