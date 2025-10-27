bac472ec56ce2deb8bfe9de0f6085de6e14fb237
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-27 09:12:18 +0900

    feat: MovieDetailPage에 초기 영화 및 상세 데이터 props 추가

--- react-hybrid/src/client/pages/MovieDetailPage.tsx
+++ react-hybrid/src/client/pages/MovieDetailPage.tsx
@@ -1,19 +1,31 @@
-import { useMovieDetailModal } from '../hooks/useMovieDetailModal';
-import { useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import MovieHomePage from './MovieHomePage';
-import { moviesApi } from '../api/movies';
+import { useMovieDetailModal } from "../hooks/useMovieDetailModal";
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import MovieHomePage from "./MovieHomePage";
+import { moviesApi } from "../api/movies";
+import { MovieItem } from "../types/Movie.types";
+import { MovieDetailResponse } from "../types/MovieDetail.types";
 
-export default function MovieDetailPage() {
+export default function MovieDetailPage({
+  movies,
+  initialDetail,
+}: {
+  movies: MovieItem[];
+  initialDetail?: MovieDetailResponse;
+}) {
   return (
     <>
-      <MovieHomePage />
-      <DetailPageOpenModal />
+      <MovieHomePage movies={movies} />
+      <DetailPageOpenModal initialDetail={initialDetail} />
     </>
   );
 }
 
-function DetailPageOpenModal() {
+function DetailPageOpenModal({
+  initialDetail,
+}: {
+  initialDetail?: MovieDetailResponse;
+}) {
   const { movieId } = useParams();
   const { openMovieDetailModal } = useMovieDetailModal();
   const onceRef = useRef(false);
@@ -22,12 +34,19 @@ function DetailPageOpenModal() {
     if (movieId == null || onceRef.current === true) {
       return;
     }
+
+    if (initialDetail) {
+      onceRef.current = true;
+      openMovieDetailModal(initialDetail);
+      return;
+    }
+
     (async () => {
       onceRef.current = true;
       const movieDetail = await moviesApi.getDetail(Number(movieId));
       openMovieDetailModal(movieDetail.data);
     })();
-  }, [movieId, openMovieDetailModal]);
+  }, [movieId, openMovieDetailModal, initialDetail]);
 
   return null;
 }
