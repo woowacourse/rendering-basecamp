b1bbd840e112ca900622d66ea59f77e8e8cc6c96
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-27 09:12:50 +0900

    feat: BrowserRouter 및 SSR 초기 데이터 전달 구현

--- react-hybrid/src/client/main.tsx
+++ react-hybrid/src/client/main.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const initialData = window.__INITIAL_DATA__;
+const initialData = (window as any).__INITIAL_DATA__;
 console.log("initialData", initialData);
 
-hydrateRoot(document.getElementById("root")!, <App />);
+hydrateRoot(
+  document.getElementById("root")!,
+  <BrowserRouter>
+    <App
+      initialMovies={initialData?.movies || []}
+      initialDetail={initialData?.detail}
+    />
+  </BrowserRouter>
+);
