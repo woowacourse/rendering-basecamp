090dd6155889a4258a6595f76ab7b11e19148e4b
guesung <gueit214@naver.com>
2025-10-28 11:55:33 +0900

    refactor: 불필요한 console.log 제거

--- react-hybrid/src/client/hooks/queries/useMovieDetail.ts
+++ react-hybrid/src/client/hooks/queries/useMovieDetail.ts
@@ -7,18 +7,16 @@ import { MovieDetailResponse } from '../../types/MovieDetail.types';
  */
 export const useMovieDetail = (
   id: number,
-  movieServerData?: MovieDetailResponse,
+  movieServerData?: MovieDetailResponse
 ) => {
   const [data, setData] = useState<MovieDetailResponse | null>(movieServerData);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    console.log(id);
     if (!id) return;
 
     const fetchMovieDetail = async () => {
-      console.log(id);
       setIsLoading(true);
       setError(null);
 
@@ -29,7 +27,7 @@ export const useMovieDetail = (
         setError(
           err instanceof Error
             ? err
-            : new Error('영화 상세 정보를 불러오는데 실패했습니다.'),
+            : new Error('영화 상세 정보를 불러오는데 실패했습니다.')
         );
       } finally {
         setIsLoading(false);
