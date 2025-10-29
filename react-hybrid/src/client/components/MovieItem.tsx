b0fa93c83768c54e0dde869b10bdc570c9db4bb1
mun-kyeong <horse6953@naver.com>
2025-10-29 19:56:05 +0900

    fix: MovieItem에서 onClick 핸들러 추가 및 MovieList에서 클릭 처리 로직 통합

--- react-hybrid/src/client/components/MovieItem.tsx
+++ react-hybrid/src/client/components/MovieItem.tsx
@@ -3,35 +3,38 @@ import type { MovieItem as MovieItemType } from "../types/Movie.types";
 interface MovieItemProps {
   movie: MovieItemType;
   ref?: React.Ref<HTMLLIElement>;
+  onClick: (movie: MovieItemType) => void;
 }
 
-export const MovieItem = ({ movie, ref }: MovieItemProps) => {
+export const MovieItem = ({ movie, ref, onClick }: MovieItemProps) => {
   const { title, poster_path, vote_average } = movie;
 
   const imageUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
     : "/images/no_image.png";
 
+  const handleClick = () => {
+    onClick(movie);
+  };
+
   return (
-    <a href={`/detail/${movie.id}`}>
-      <li ref={ref} className="movie-item" data-index={movie.id}>
-        <div className="item">
-          <img
-            className="thumbnail"
-            src={imageUrl}
-            alt={title}
-            loading="lazy"
-          />
-          <div className="item-desc">
-            <p className="rate">
-              <img src="/images/star_empty.png" className="star" />
-              <span>{vote_average.toFixed(1)}</span>
-            </p>
-            <strong>{title}</strong>
-          </div>
+    <li
+      ref={ref}
+      className="movie-item"
+      data-index={movie.id}
+      onClick={handleClick}
+    >
+      <div className="item">
+        <img className="thumbnail" src={imageUrl} alt={title} loading="lazy" />
+        <div className="item-desc">
+          <p className="rate">
+            <img src="/images/star_empty.png" className="star" />
+            <span>{vote_average.toFixed(1)}</span>
+          </p>
+          <strong>{title}</strong>
         </div>
-      </li>
-    </a>
+      </div>
+    </li>
   );
 };
 
--- react-hybrid/src/client/components/MovieList.tsx
+++ react-hybrid/src/client/components/MovieList.tsx
@@ -4,13 +4,26 @@ import type { MovieItem as MovieItemType } from "../types/Movie.types";
 import { moviesApi } from "../api/movies";
 
 export const MovieList = ({ movies }: { movies: MovieItemType[] }) => {
+  const { openMovieDetailModal } = useMovieDetailModal();
+
+  const handleMovieClick = async (movie: MovieItemType) => {
+    window.history.pushState(null, "", `/detail/${movie.id}`);
+    const movieDetail = await moviesApi.getDetail(movie.id);
+    await openMovieDetailModal(movieDetail.data);
+    window.history.pushState(null, "", "/");
+  };
+
   return (
     <main>
       <section className="container">
         <h2 className="text-2xl font-bold mb-64">지금 인기 있는 영화</h2>
         <ul className="thumbnail-list">
           {movies.map((movie) => (
-            <MovieItem key={movie.id} movie={movie} />
+            <MovieItem
+              key={movie.id}
+              movie={movie}
+              onClick={handleMovieClick}
+            />
           ))}
         </ul>
       </section>
