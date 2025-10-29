0fe08c0191ad440423c0a87b0f178cef80aa04b3
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-29 09:31:08 +0900

    fix: webpack.server.config.js에 아래 설정을 추가하여 서버 런타임 환경에서도 시스템 환경 변수를 인식하도록 수정

--- react-hybrid/webpack.server.config.js
+++ react-hybrid/webpack.server.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -42,4 +43,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv({ systemvars: true }),
+  ],
 };
