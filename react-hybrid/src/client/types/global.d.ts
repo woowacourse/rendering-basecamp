e43dad87fb89c4c4abe81480c2a94b9bdbef51bd
keemsebeen <sebin0580@gmail.com>
2025-10-29 14:31:45 +0900

    feat: __INITIAL_DATA__ 타입 지정

--- react-hybrid/src/client/types/global.d.ts
+++ react-hybrid/src/client/types/global.d.ts
@@ -1,8 +1,11 @@
+import { MovieItem } from "./Movie.types";
+import { MovieDetailResponse } from "./MovieDetail.types";
+
 declare global {
   interface Window {
     __INITIAL_DATA__: {
-      movies: any[];
-      movieDetail: any;
+      movies: MovieItem[];
+      movieDetail?: MovieDetailResponse;
     };
   }
 }
